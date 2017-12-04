2f770a90c47dad3c96d798d6f7db47befe8756b7
Bjørn Remseth <rmz@comoyo.com>
2017-12-04 17:13:24 +0100

    Some whitepace changes to trigger a build.

--- src/main/java/org/ostelco/App.java
+++ src/main/java/org/ostelco/App.java
@@ -4,8 +4,8 @@ package org.ostelco;
  * Hello world!
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!");
-	System.out.println( "Hello Upside Down");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+	System.out.println("Hello Upside Down");
     }
 }
