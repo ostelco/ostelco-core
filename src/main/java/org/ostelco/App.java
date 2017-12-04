1228cb122153ce4d9d54f85aabd87b6a607829cd
Bjørn Remseth <rmz@comoyo.com>
2017-12-04 13:09:23 +0100

    Trying out a pull request

--- src/main/java/org/ostelco/App.java
+++ src/main/java/org/ostelco/App.java
@@ -3,10 +3,10 @@ package org.ostelco;
 /**
  * Hello world!
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!");
+	if (false)
+	    System.out.println( "Hello Upside Down");
     }
 }
