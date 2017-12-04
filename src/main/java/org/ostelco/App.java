c6746fcfc6fdfef82e541d82760d364c5673e408
Bjørn Remseth <rmz@comoyo.com>
2017-12-04 13:22:37 +0100

    Fixing issue that was reported by codacity

--- src/main/java/org/ostelco/App.java
+++ src/main/java/org/ostelco/App.java
@@ -6,7 +6,6 @@ package org.ostelco;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!");
-	if (false)
-	    System.out.println( "Hello Upside Down");
+	System.out.println( "Hello Upside Down");
     }
 }
