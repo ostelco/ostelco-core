242d4a720424598e0e33d67dde734c984674e156
Bjørn Remseth <rmz@comoyo.com>
2017-12-09 19:19:39 +0100

    Whitespaces and comments

--- src/main/java/com/telenordigital/prime/storage/entities/Product.java
+++ src/main/java/com/telenordigital/prime/storage/entities/Product.java
@@ -6,10 +6,23 @@ import static com.google.common.base.Preconditions.checkNotNull;
 // XXX Do the singelton thing.  It's ugly, but for now that's ok.
 
 public final class Product {
+
+    /**
+     * A "Stock Keeping Unit" that is assumed to be a primary key for products.
+     */
     private final String sku;
+
+    /**
+     * A description intended to be useful for the consumer.
+     */
     private final Object productDescription;
 
 
+    /**
+     *
+     * @param sku A "Stock Keeping Unit" that is assumed to be a primary key for products.
+     * @param productDescription A description intended to be useful for the consumer.
+     */
     public Product(final String sku, final Object productDescription) {
         this.sku = checkNotNull(sku);
         this.productDescription = checkNotNull(productDescription);
@@ -34,10 +47,9 @@ public final class Product {
 
     @Override
     public String toString() {
-        return "Product{" +
-                "sku='" + sku + '\'' +
-                ", productDescription=" + productDescription +
-                '}';
+        return "Product{"
+                + "sku='" + sku + '\''
+                + ", productDescription=" + productDescription
+                + '}';
     }
-
 }
