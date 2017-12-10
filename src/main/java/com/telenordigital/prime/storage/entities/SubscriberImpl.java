02567b8f5e569febede0769cbf58384b7dd079c9
Bjørn Remseth <rmz@comoyo.com>
2017-12-10 22:22:15 +0100

    Steps towards fewer style issues

--- src/main/java/com/telenordigital/prime/storage/entities/SubscriberImpl.java
+++ src/main/java/com/telenordigital/prime/storage/entities/SubscriberImpl.java
@@ -1,16 +1,12 @@
 package com.telenordigital.prime.storage.entities;
 
-import com.telenordigital.prime.storage.AsMappable;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import static com.google.common.base.Preconditions.checkNotNull;
 
 
 public final class SubscriberImpl implements Subscriber {
 
     private String fbKey;
+
     private String msisdn;
 
     private long noOfBytesLeft;
@@ -34,7 +30,6 @@ public final class SubscriberImpl implements Subscriber {
         return msisdn;
     }
 
-
     public void setFbKey(final String fbKey) {
         this.fbKey = checkNotNull(fbKey);
     }
@@ -44,7 +39,7 @@ public final class SubscriberImpl implements Subscriber {
     }
 
     public void setNoOfBytesLeft(final long noOfBytesLeft) {
-        this.noOfBytesLeft = checkNotNull(noOfBytesLeft);
+        this.noOfBytesLeft = noOfBytesLeft;
     }
 
 
@@ -59,23 +54,30 @@ public final class SubscriberImpl implements Subscriber {
 
     @Override
     public boolean equals(final Object o) {
-        if (this == o) {return true;}
-        if (o == null || getClass() != o.getClass()) {return false;}
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         final SubscriberImpl that = (SubscriberImpl) o;
 
-        if (getNoOfBytesLeft() != that.getNoOfBytesLeft()) {return false;}
-        if (!getFbKey().equals(that.getFbKey())) {return false;}
-        return getMsisdn().equals(that.getMsisdn());
+        if (getNoOfBytesLeft() != that.getNoOfBytesLeft()) {
+            return false;
+        }
+        return getFbKey().equals(that.getFbKey()) && getMsisdn().equals(that.getMsisdn());
     }
 
     @Override
     public int hashCode() {
         int result = getFbKey().hashCode();
         final int usefulSmallishPrime = 31;
-        result = usefulSmallishPrime * result + getMsisdn().hashCode();
+        result *= usefulSmallishPrime;
+        result += getMsisdn().hashCode();
         final int noOfBitsInAnInteger = 32;
-        result = usefulSmallishPrime * result + (int) (getNoOfBytesLeft() ^ (getNoOfBytesLeft() >>> noOfBitsInAnInteger));
+        result *= usefulSmallishPrime;
+        result += (int) (getNoOfBytesLeft() ^ (getNoOfBytesLeft() >>> noOfBitsInAnInteger));
         return result;
     }
 }
