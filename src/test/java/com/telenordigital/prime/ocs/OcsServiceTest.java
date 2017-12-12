48dad4552f89bd3cca93f068915112ba1ff460c2
Bjørn Remseth <rmz@comoyo.com>
2017-12-12 09:53:40 +0100

    Empty shell of a test

---
+++ src/test/java/com/telenordigital/prime/ocs/OcsServiceTest.java
@@ -0,0 +1,30 @@
+package com.telenordigital.prime.ocs;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OcsServiceTest {
+
+    private OcsService service;
+
+    @Before
+    public void setUp() {
+        // service = new OcsService();
+    }
+
+    @Test
+    public void onEvent() {
+    }
+
+    @Test
+    public void fetchDataBucket() {
+    }
+
+    @Test
+    public void returnUnusedData() {
+    }
+
+    @Test
+    public void activate() {
+    }
+}
