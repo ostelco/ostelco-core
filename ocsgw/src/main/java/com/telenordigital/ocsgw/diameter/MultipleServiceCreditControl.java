90da1368b7a7ad697dc7771bee6ab9dd66fc9cd7
Martin Cederlof <mape@comoyo.com>
2018-01-24 14:12:43 +0100

    Removed public setters

--- ocsgw/src/main/java/com/telenordigital/ocsgw/diameter/MultipleServiceCreditControl.java
+++ ocsgw/src/main/java/com/telenordigital/ocsgw/diameter/MultipleServiceCreditControl.java
@@ -27,35 +27,77 @@ public class MultipleServiceCreditControl {
     public long getRequestedUnits() {
         return requestedUnits;
     }
-    private void setRequestedUnits(long requestedUnits) {
-        this.requestedUnits = requestedUnits;
-    }
 
     public long getReservedUnits() {
         return reservedUnits;
     }
-    public void setReservedUnits(long reservedUnits) {
-        this.reservedUnits = reservedUnits;
+
+    public long getUsedUnitsTotal() {
+        return usedUnitsTotal;
     }
 
-    public long getUsedUnitsTotal() { return usedUnitsTotal; }
-    public void setUsedUnitsTotal(long usedUnitsTotal) { this.usedUnitsTotal = usedUnitsTotal; }
+    public long getUsedUnitsInput() {
+        return usedUnitsInput;
+    }
 
-    public long getUsedUnitsInput() { return usedUnitsInput; }
-    public void setUsedUnitsInput(long usedUnitsInput) { this.usedUnitsInput = usedUnitsInput; }
+    public long getUsedUnitsOutput() {
+        return usedUnitsOutput;
+    }
 
-    public long getUsedUnitsOutput() { return usedUnitsOutput; }
-    public void setUsedUnitsOutput(long usedUnitsOutput) { this.usedUnitsOutput = usedUnitsOutput; }
+    public long getRatingGroup() {
+        return ratingGroup;
+    }
 
-    public long getRatingGroup() { return ratingGroup; }
-    public void setRatingGroup(long ratingGroup) { this.ratingGroup = ratingGroup; }
+    public int getServiceIdentifier() {
+        return serviceIdentifier;
+    }
 
-    public int getServiceIdentifier() { return serviceIdentifier; }
-    public void setServiceIdentifier(int serviceIdentifier) { this.serviceIdentifier = serviceIdentifier; }
+    public long getGrantedServiceUnit() {
+        return grantedServiceUnit;
+    }
 
-    public long getGrantedServiceUnit() { return grantedServiceUnit; }
-    public void setGrantedServiceUnit(long grantedServiceUnit) { this.grantedServiceUnit = grantedServiceUnit; }
+    public void parseAvps(AvpSet serviceControl) {
+        try {
+            Optional<Avp> ratingGroup = Optional.ofNullable(serviceControl.getAvp(Avp.RATING_GROUP));
+            if (ratingGroup.isPresent()) {
+                this.ratingGroup = ratingGroup.get().getInteger32();
+            }
 
+            Optional<Avp> serviceIdentifier = Optional.ofNullable(serviceControl.getAvp(Avp.SERVICE_IDENTIFIER));
+            if (serviceIdentifier.isPresent()) {
+                this.serviceIdentifier = serviceIdentifier.get().getInteger32();
+            }
+
+            Optional<Avp> requestedServiceUnits = Optional.ofNullable(serviceControl.getAvp(Avp.REQUESTED_SERVICE_UNIT));
+            if (requestedServiceUnits.isPresent()) {
+                this.requestedUnits = requestedServiceUnits.get().getGrouped().getAvp(Avp.CC_TOTAL_OCTETS).getUnsigned64();
+            }
+
+            Optional<AvpSet> usedServiceUnitsSet = Optional.ofNullable(serviceControl.getAvps(Avp.USED_SERVICE_UNIT));
+            if (usedServiceUnitsSet.isPresent()) {
+                for (Avp nextUsedServiceUnit : usedServiceUnitsSet.get()) {
+                    AvpSet usedServiceUnit = nextUsedServiceUnit.getGrouped();
+
+                    Optional<Avp> totalOctets = Optional.ofNullable(usedServiceUnit.getAvp(Avp.CC_TOTAL_OCTETS));
+                    if (totalOctets.isPresent()) {
+                        this.usedUnitsTotal = totalOctets.get().getUnsigned64();
+                    }
+
+                    Optional<Avp> InputOctets = Optional.ofNullable(usedServiceUnit.getAvp(Avp.CC_INPUT_OCTETS));
+                    if (InputOctets.isPresent()) {
+                        this.usedUnitsInput = InputOctets.get().getUnsigned64();
+                    }
+
+                    Optional<Avp> outputOctets = Optional.ofNullable(usedServiceUnit.getAvp(Avp.CC_OUTPUT_OCTETS));
+                    if (outputOctets.isPresent()) {
+                        this.usedUnitsOutput = outputOctets.get().getUnsigned64();
+                    }
+                }
+            }
+        } catch (AvpDataException e) {
+            logger.warn("Failed to parse Multiple-Service-Credit-Control", e);
+        }
+    }
 
     @Override
     public String toString() {
@@ -72,46 +114,7 @@ public class MultipleServiceCreditControl {
         return s;
     }
 
-    public void parseAvps(AvpSet serviceControl) {
-        try {
-            Optional<Avp> ratingGroup = Optional.ofNullable(serviceControl.getAvp(Avp.RATING_GROUP));
-            if (ratingGroup.isPresent()) {
-                this.setRatingGroup(ratingGroup.get().getInteger32());
-            }
-
-            Optional<Avp> serviceIdentifier = Optional.ofNullable(serviceControl.getAvp(Avp.SERVICE_IDENTIFIER));
-            if (serviceIdentifier.isPresent()) {
-                this.setServiceIdentifier(serviceIdentifier.get().getInteger32());
-            }
-
-            Optional<Avp> requestedServiceUnits = Optional.ofNullable(serviceControl.getAvp(Avp.REQUESTED_SERVICE_UNIT));
-            if (requestedServiceUnits.isPresent()) {
-                this.setRequestedUnits(requestedServiceUnits.get().getGrouped().getAvp(Avp.CC_TOTAL_OCTETS).getUnsigned64());
-            }
-
-            Optional<AvpSet> usedServiceUnitsSet = Optional.ofNullable(serviceControl.getAvps(Avp.USED_SERVICE_UNIT));
-            if (usedServiceUnitsSet.isPresent()) {
-                for (Avp nextUsedServiceUnit : usedServiceUnitsSet.get()) {
-                    AvpSet usedServiceUnit = nextUsedServiceUnit.getGrouped();
-
-                    Optional<Avp> totalOctets = Optional.ofNullable(usedServiceUnit.getAvp(Avp.CC_TOTAL_OCTETS));
-                    if (totalOctets.isPresent()) {
-                        this.setUsedUnitsTotal(totalOctets.get().getUnsigned64());
-                    }
-
-                    Optional<Avp> InputOctets = Optional.ofNullable(usedServiceUnit.getAvp(Avp.CC_INPUT_OCTETS));
-                    if (InputOctets.isPresent()) {
-                        this.setUsedUnitsInput(InputOctets.get().getUnsigned64());
-                    }
-
-                    Optional<Avp> outputOctets = Optional.ofNullable(usedServiceUnit.getAvp(Avp.CC_OUTPUT_OCTETS));
-                    if (outputOctets.isPresent()) {
-                        this.setUsedUnitsOutput(outputOctets.get().getUnsigned64());
-                    }
-                }
-            }
-        } catch (AvpDataException e) {
-            logger.warn("Failed to parse Multiple-Service-Credit-Control", e);
-        }
+    public void setGrantedServiceUnit(long bytes) {
+        this.grantedServiceUnit = bytes;
     }
 }
