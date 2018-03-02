2e7c5de2ca9615a3cd3f5acf5487f13a97144143
Prasanth Ullattil <prasanth@telenordigital.com>
2018-03-02 14:00:40 +0100

    Add debug-agent for google cloud

--- uid-manger/src/server.ts
+++ uid-manger/src/server.ts
@@ -1,5 +1,7 @@
+import * as DebugAgent from "@google-cloud/debug-agent";
 import app from "./app";
 
+DebugAgent.start();
 const server = app.listen(app.get("port"), () => {
   console.log(
     "  App is running at http://localhost:%d in %s mode",
