00e6addc803ea7ba1958cd809797f964f51a8554
Prasanth Ullattil <prasanth@telenordigital.com>
2018-03-06 21:18:09 +0100

    Change the API name from token to pseudonym

--- uid-manger/src/pseudonyms.ts
+++ uid-manger/src/pseudonyms.ts
@@ -4,68 +4,76 @@ import * as crypto from "crypto";
 import * as endOfMonth from "date-fns/end_of_month";
 import * as startOfMonth from "date-fns/start_of_month";
 
-export interface TokenObject {
+export interface PseudonymObject {
   end: number;
   start: number;
-  token: string;
+  pseudonym: string;
   userId: string;
 }
-const tokenObjectTypeName = "TokenObject_test";
+const pseudonymObjectTypeName = "PseudonymObject_test";
 
-export function getKeyForTokenObject(msisdn: string, start: Date, datastore: Datastore) {
+export function getKeyForPseudonymObject(msisdn: string, start: Date, datastore: Datastore) {
   const keyIdentifier: string = `${msisdn}--${start.getTime()}`;
-  return datastore.key([tokenObjectTypeName, keyIdentifier]);
+  return datastore.key([pseudonymObjectTypeName, keyIdentifier]);
 }
 
-export function createTokenObject(msisdn: string, userId: string, timestamp: number): TokenObject {
+export function createPseudonymObject(
+  msisdn: string,
+  userId: string,
+  timestamp: number
+): PseudonymObject {
   const hash = crypto.createHash("sha256");
   const start = startOfMonth(timestamp);
   const end = endOfMonth(timestamp);
   const hashData: string = `${userId}--${start.getTime()}`;
   hash.update(hashData);
-  const token = hash.digest("hex");
-  const tokenObject: TokenObject = {
+  const pseudonym = hash.digest("hex");
+  const pseudonymObject: PseudonymObject = {
     end: end.getTime(),
+    pseudonym,
     start: start.getTime(),
-    token,
     userId
   };
-  return tokenObject;
+  return pseudonymObject;
 }
 
-export async function generateToken(
+export async function generatePseudonym(
   msisdn: string,
   userId: string,
   timestamp: number,
   datastore: Datastore
 ) {
-  const tokenObject = createTokenObject(msisdn, userId, timestamp);
-  const tokenEntity = {
-    data: tokenObject,
-    key: getKeyForTokenObject(msisdn, startOfMonth(timestamp), datastore)
+  const pseudonymObject = createPseudonymObject(msisdn, userId, timestamp);
+  const pseudonymEntity = {
+    data: pseudonymObject,
+    key: getKeyForPseudonymObject(msisdn, startOfMonth(timestamp), datastore)
   };
-  await datastore.upsert(tokenEntity);
-  return tokenObject.token;
+  await datastore.upsert(pseudonymEntity);
+  return pseudonymObject.pseudonym;
 }
 
-export function getToken(msisdn: string, timestamp: number, datastore: Datastore): Promise<string> {
+export function getPseudonym(
+  msisdn: string,
+  timestamp: number,
+  datastore: Datastore
+): Promise<string> {
   const start = startOfMonth(timestamp);
-  return datastore.get(getKeyForTokenObject(msisdn, start, datastore)).then(data => {
-    return (data[0] as TokenObject).token;
+  return datastore.get(getKeyForPseudonymObject(msisdn, start, datastore)).then(data => {
+    return (data[0] as PseudonymObject).pseudonym;
   });
 }
 
-export function findUserId(token: string, datastore: Datastore): Promise<string> {
+export function findUserId(pseudonym: string, datastore: Datastore): Promise<string> {
   const query = datastore
-    .createQuery(tokenObjectTypeName)
-    .filter("token", "=", token)
+    .createQuery(pseudonymObjectTypeName)
+    .filter("pseudonym", "=", pseudonym)
     .limit(1);
   return query.run().then(data => {
-    return (data[0][0] as TokenObject).userId;
+    return (data[0][0] as PseudonymObject).userId;
   });
 }
 
-export class TokenAPIHandler {
+export class PseudonymAPIHandler {
   private datastore: Datastore;
 
   constructor(datastore) {
--- uid-manger/test/pseudonyms.test.ts
+++ uid-manger/test/pseudonyms.test.ts
@@ -2,7 +2,7 @@ import * as Datastore from "@google-cloud/datastore";
 import * as crypto from "crypto";
 import * as startOfMonth from "date-fns/start_of_month";
 import Emulator = require("google-datastore-emulator");
-import * as tokens from "../src/tokens";
+import * as pseudonyms from "../src/pseudonyms";
 
 let emulator;
 process.env.GCLOUD_PROJECT = "test";
@@ -23,28 +23,28 @@ const msisdn = "4792624130";
 const userId = "12-23-34-45";
 const timestamp = Date.now();
 
-it("Check hash of token", async () => {
+it("Check hash of pseudonym", async () => {
   const datastore = new Datastore({});
   const hash = crypto.createHash("sha256");
   const start = startOfMonth(timestamp);
   const hashData: string = `${userId}--${start.getTime()}`;
   hash.update(hashData);
-  const token = hash.digest("hex");
+  const pseudonym = hash.digest("hex");
 
-  const data = await tokens.generateToken(msisdn, userId, timestamp, datastore);
-  expect(data).toEqual(token);
+  const data = await pseudonyms.generatePseudonym(msisdn, userId, timestamp, datastore);
+  expect(data).toEqual(pseudonym);
 });
 
-it("Get token for msisdn", async () => {
+it("Get pseudonym for msisdn", async () => {
   const datastore = new Datastore({});
-  const token = await tokens.generateToken(msisdn, userId, timestamp, datastore);
-  const data = await tokens.getToken(msisdn, timestamp, datastore);
-  expect(data).toEqual(token);
+  const pseudonym = await pseudonyms.generatePseudonym(msisdn, userId, timestamp, datastore);
+  const data = await pseudonyms.getPseudonym(msisdn, timestamp, datastore);
+  expect(data).toEqual(pseudonym);
 });
 
-it("Get userId from token", async () => {
+it("Get userId from pseudonym", async () => {
   const datastore = new Datastore({});
-  const token = await tokens.generateToken(msisdn, userId, timestamp, datastore);
-  const data = await tokens.findUserId(token, datastore);
+  const pseudonym = await pseudonyms.generatePseudonym(msisdn, userId, timestamp, datastore);
+  const data = await pseudonyms.findUserId(pseudonym, datastore);
   expect(data).toEqual(userId);
 });
