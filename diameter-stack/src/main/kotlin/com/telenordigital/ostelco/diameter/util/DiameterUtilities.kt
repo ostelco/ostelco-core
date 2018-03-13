d52a14b8ed224ce96831478b40451c4433b82bc8
Martin Cederlof <mape@comoyo.com>
2018-03-13 14:36:59 +0100

    Remove white space

--- diameter-stack/src/main/kotlin/com/telenordigital/ostelco/diameter/util/DiameterUtilities.kt
+++ diameter-stack/src/main/kotlin/com/telenordigital/ostelco/diameter/util/DiameterUtilities.kt
@@ -34,7 +34,6 @@ class DiameterUtilities {
                 } catch (e: AvpDataException) {
                     // Failed to ungroup... ignore then...
                 }
-
             }
         }
     }
