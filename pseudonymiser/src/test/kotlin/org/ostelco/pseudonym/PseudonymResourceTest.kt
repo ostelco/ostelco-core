ac35d7b79e09e67ee8ecfc97dec75c9b1f94b9dc
Vihang Patil <vihang.patil@gmail.com>
2018-03-22 17:59:10 +0100

    Passing unit test

--- pseudonymiser/src/test/kotlin/org/ostelco/pseudonym/PseudonymResourceTest.kt
+++ pseudonymiser/src/test/kotlin/org/ostelco/pseudonym/PseudonymResourceTest.kt
@@ -1,40 +1,45 @@
 package org.ostelco.pseudonym
 
-import org.ostelco.pseudonym.resources.PseudonymResource
+import com.google.cloud.datastore.Datastore
+import com.google.cloud.datastore.testing.LocalDatastoreHelper
 import io.dropwizard.testing.junit.ResourceTestRule
+import org.junit.ClassRule
 import org.junit.Test
+import org.ostelco.pseudonym.resources.PseudonymResource
 import javax.ws.rs.core.Response.Status
 import kotlin.test.assertEquals
-import com.google.cloud.datastore.testing.LocalDatastoreHelper
-import com.google.cloud.datastore.Datastore
-import org.junit.BeforeClass
+
+
 
 class PseudonymResourceTest {
 
     companion object {
-        val helper: LocalDatastoreHelper = LocalDatastoreHelper.create(1.0)
-        lateinit var datastore: Datastore
 
-        @BeforeClass @JvmStatic fun setup() {
-            // things to execute once and keep around for the class
+        private var datastore: Datastore
+
+        init {
+            val helper: LocalDatastoreHelper = LocalDatastoreHelper.create(1.0)
             helper.start()
             datastore = helper.options.service
         }
+
+        @ClassRule
+        @JvmField
+        val resources = ResourceTestRule.builder()
+                .addResource(PseudonymResource(datastore))
+                .build()
     }
 
     @Test
     fun testPseudonymResourceForMissingHeader() {
         System.out.println("testPseudonymResourceForMissingHeader")
-        val resources = ResourceTestRule.builder()
-                .addResource(PseudonymResource(datastore))
-                .build()
-        val i = 10;
+
         val statusCode = resources
                 ?.target("/pseudonym/current/")
                 ?.request()
                 ?.get()
                 ?.status ?: -1
 
-        assertEquals(Status.INTERNAL_SERVER_ERROR.statusCode, statusCode)
+        assertEquals(Status.NOT_FOUND.statusCode, statusCode)
     }
 }
