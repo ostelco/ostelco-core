9b87c0d78a42228951f7a02371baa3fcac375731
Vihang Patil <vihang.patil@gmail.com>
2018-03-23 03:50:41 +0100

    Passing ocsgw unit test

--- diameter-stack/src/main/kotlin/org/ostelco/diameter/builder/Builder.kt
+++ diameter-stack/src/main/kotlin/org/ostelco/diameter/builder/Builder.kt
@@ -1,2 +1,39 @@
 package org.ostelco.diameter.builder
 
+import org.jdiameter.api.AvpSet
+import org.jdiameter.api.URI
+import java.net.InetAddress
+import java.util.*
+
+/**
+ * DSL style helper class to populate values into {@link org.jdiameter.api.AvpSet}
+ */
+fun set(avpSet: AvpSet, init: AvpSetContext.() -> Unit) {
+    val avpSetContext = AvpSetContext(avpSet)
+    avpSetContext.init()
+}
+
+class AvpSetContext(val avpSet: AvpSet) {
+    fun avp(
+            avpCode: Int, value: Any, vendorId: Long = 0,
+            mFlag: Boolean = true, pFlag: Boolean = false, asOctetString: Boolean = false) {
+        when (value) {
+            is ByteArray -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag)
+            is Int -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag)
+            is Long -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag)
+            is Float -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag)
+            is Double -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag)
+            is String -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag, asOctetString)
+            is URI -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag)
+            is InetAddress -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag)
+            is Date -> avpSet.addAvp(avpCode, value, vendorId, mFlag, pFlag)
+        }
+    }
+
+    fun group(avpCode: Int, vendorId:Long = 0, mFlag: Boolean = true, pFlag: Boolean = false,
+              init: AvpSetContext.() -> Unit) {
+        val subContext = AvpSetContext(avpSet.addGroupedAvp(avpCode, vendorId, mFlag, pFlag))
+        subContext.init()
+    }
+}
+
--- diameter-test/src/main/kotlin/org/ostelco/diameter/test/TestHelper.kt
+++ diameter-test/src/main/kotlin/org/ostelco/diameter/test/TestHelper.kt
@@ -1,129 +1,105 @@
 package org.ostelco.diameter.test
 
 import org.jdiameter.api.Avp
+import org.jdiameter.api.Avp.PS_INFORMATION
 import org.jdiameter.api.AvpSet
+import org.ostelco.diameter.builder.set
 import org.ostelco.diameter.model.RequestType
 import org.ostelco.diameter.model.SubscriptionType
 
-class TestHelper {
+object TestHelper {
 
-    companion object {
-        private const val VENDOR_ID_3GPP = 10415L
-        private const val MSISDN = "4747900184"
-        private const val IMSI = "242017100000228"
-        private const val APN = "panacea"
-        private const val SGSN_MCC_MNC = "24201"
-        private const val CALLED_STATION_ID = 30
-        private const val BUCKET_SIZE = 500L
+    private const val VENDOR_ID_3GPP = 10415L
+    private const val IMSI = "242017100000228"
+    private const val APN = "panacea"
+    private const val SGSN_MCC_MNC = "24201"
+    private const val CALLED_STATION_ID = 30
+
+    private fun build(ccrAvps: AvpSet, requestType: Int, requestNumber: Int, msisdn: String, bucketSize: Long) {
+
+        set(ccrAvps) {
+            avp(Avp.CC_REQUEST_TYPE, requestType)
+            avp(Avp.CC_REQUEST_NUMBER, requestNumber)
+
+            group(Avp.SUBSCRIPTION_ID) {
+                avp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_E164.ordinal)
+                avp(Avp.SUBSCRIPTION_ID_DATA, msisdn, pFlag = true)
+            }
+            group(Avp.SUBSCRIPTION_ID) {
+                avp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_IMSI.ordinal)
+                avp(Avp.SUBSCRIPTION_ID_DATA, IMSI, pFlag = true)
+            }
+
+            avp(Avp.MULTIPLE_SERVICES_INDICATOR, 1, pFlag = true)
+
+            group(Avp.MULTIPLE_SERVICES_CREDIT_CONTROL) {
+                avp(Avp.RATING_GROUP, 10, pFlag = true)
+                avp(Avp.SERVICE_IDENTIFIER_CCA, 1, pFlag = true)
+
+                group(Avp.REQUESTED_SERVICE_UNIT) {
+                    avp(Avp.CC_TOTAL_OCTETS, bucketSize, pFlag = true)
+                    avp(Avp.CC_INPUT_OCTETS, 0L, pFlag = true)
+                    avp(Avp.CC_OUTPUT_OCTETS, 0L, pFlag = true)
+                }
+            }
+
+            group(Avp.SERVICE_INFORMATION, vendorId = VENDOR_ID_3GPP) {
+                group(PS_INFORMATION, vendorId = VENDOR_ID_3GPP) {
+                    avp(CALLED_STATION_ID, APN, pFlag = true)
+                    avp(Avp.GPP_SGSN_MCC_MNC, SGSN_MCC_MNC, vendorId = VENDOR_ID_3GPP, asOctetString = true)
+                }
+            }
+        }
     }
 
-    private fun init(ccrAvps: AvpSet) {
-
-        ccrAvps.addAvp(Avp.CC_REQUEST_TYPE, RequestType.INITIAL_REQUEST, true, false)
-        ccrAvps.addAvp(Avp.CC_REQUEST_NUMBER, 0, true, false)
-
-        val subscriptionId = ccrAvps.addGroupedAvp(Avp.SUBSCRIPTION_ID)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_E164.ordinal)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_DATA, MSISDN, false)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_IMSI.ordinal)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_DATA, IMSI, false)
-
-        ccrAvps.addAvp(Avp.MULTIPLE_SERVICES_INDICATOR, 1)
-
-        val mscc = ccrAvps.addGroupedAvp(Avp.MULTIPLE_SERVICES_CREDIT_CONTROL)
-        mscc.addAvp(Avp.RATING_GROUP, 10)
-        mscc.addAvp(Avp.SERVICE_IDENTIFIER_CCA, 1)
-        val requestedServiceUnits = mscc.addGroupedAvp(Avp.REQUESTED_SERVICE_UNIT)
-        requestedServiceUnits.addAvp(Avp.CC_TOTAL_OCTETS, BUCKET_SIZE)
-        requestedServiceUnits.addAvp(Avp.CC_INPUT_OCTETS, 0L)
-        requestedServiceUnits.addAvp(Avp.CC_OUTPUT_OCTETS, 0L)
-
-        val serviceInformation = ccrAvps.addGroupedAvp(Avp.SERVICE_INFORMATION, VENDOR_ID_3GPP, true, false)
-        val psInformation = serviceInformation.addGroupedAvp(Avp.PS_INFORMATION, VENDOR_ID_3GPP, true, false)
-        psInformation.addAvp(CALLED_STATION_ID, APN, false)
-        psInformation.addAvp(Avp.GPP_SGSN_MCC_MNC, SGSN_MCC_MNC, VENDOR_ID_3GPP, true, false, true)
+    fun init(ccrAvps: AvpSet, msisdn: String, bucketSize: Long) {
+        build(ccrAvps, RequestType.INITIAL_REQUEST, requestNumber = 0, msisdn = msisdn, bucketSize = bucketSize)
     }
 
-    private fun update(ccrAvps: AvpSet) {
-
-        ccrAvps.addAvp(Avp.CC_REQUEST_TYPE, RequestType.UPDATE_REQUEST, true, false)
-        ccrAvps.addAvp(Avp.CC_REQUEST_NUMBER, 1, true, false)
-
-        val subscriptionId = ccrAvps.addGroupedAvp(Avp.SUBSCRIPTION_ID)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_E164.ordinal)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_DATA, MSISDN, false)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_IMSI.ordinal)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_DATA, IMSI, false)
-
-        ccrAvps.addAvp(Avp.MULTIPLE_SERVICES_INDICATOR, 1)
-
-        val mscc = ccrAvps.addGroupedAvp(Avp.MULTIPLE_SERVICES_CREDIT_CONTROL)
-        mscc.addAvp(Avp.RATING_GROUP, 10)
-        mscc.addAvp(Avp.SERVICE_IDENTIFIER_CCA, 1)
-        val requestedServiceUnits = mscc.addGroupedAvp(Avp.REQUESTED_SERVICE_UNIT)
-        requestedServiceUnits.addAvp(Avp.CC_TOTAL_OCTETS, BUCKET_SIZE)
-        requestedServiceUnits.addAvp(Avp.CC_INPUT_OCTETS, 0L)
-        requestedServiceUnits.addAvp(Avp.CC_OUTPUT_OCTETS, 0L)
-
-        val serviceInformation = ccrAvps.addGroupedAvp(Avp.SERVICE_INFORMATION, VENDOR_ID_3GPP, true, false)
-        val psInformation = serviceInformation.addGroupedAvp(Avp.PS_INFORMATION, VENDOR_ID_3GPP, true, false)
-        psInformation.addAvp(CALLED_STATION_ID, APN, false)
-        psInformation.addAvp(Avp.GPP_SGSN_MCC_MNC, SGSN_MCC_MNC, VENDOR_ID_3GPP, true, false, true)
+    fun initNoCredit(ccrAvps: AvpSet, bucketSize: Long) {
+        build(ccrAvps, RequestType.INITIAL_REQUEST, requestNumber = 0, msisdn = "4333333333", bucketSize = bucketSize)
     }
 
-    fun terminate(ccrAvps: AvpSet) {
-
-        ccrAvps.addAvp(Avp.CC_REQUEST_TYPE, RequestType.TERMINATION_REQUEST, true, false)
-        ccrAvps.addAvp(Avp.CC_REQUEST_NUMBER, 2, true, false)
-
-        val subscriptionId = ccrAvps.addGroupedAvp(Avp.SUBSCRIPTION_ID)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_E164.ordinal)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_DATA, MSISDN, false)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_IMSI.ordinal)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_DATA, IMSI, false)
-
-        ccrAvps.addAvp(Avp.TERMINATION_CAUSE, 1, true, false) // 1 = DIAMETER_LOGOUT
-
-        val mscc = ccrAvps.addGroupedAvp(Avp.MULTIPLE_SERVICES_CREDIT_CONTROL)
-        mscc.addAvp(Avp.RATING_GROUP, 10)
-        mscc.addAvp(Avp.SERVICE_IDENTIFIER_CCA, 1)
-        val usedServiceUnits = mscc.addGroupedAvp(Avp.USED_SERVICE_UNIT)
-        usedServiceUnits.addAvp(Avp.CC_TOTAL_OCTETS, BUCKET_SIZE)
-        usedServiceUnits.addAvp(Avp.CC_INPUT_OCTETS, 0L)
-        usedServiceUnits.addAvp(Avp.CC_OUTPUT_OCTETS, 0L)
-        usedServiceUnits.addAvp(Avp.CC_SERVICE_SPECIFIC_UNITS, 0L)
-        mscc.addAvp(Avp.REPORTING_REASON, 2, VENDOR_ID_3GPP, true, false) // 2 = FINAL
-
-        val serviceInformation = ccrAvps.addGroupedAvp(Avp.SERVICE_INFORMATION, VENDOR_ID_3GPP, true, false)
-        val psInformation = serviceInformation.addGroupedAvp(Avp.PS_INFORMATION, VENDOR_ID_3GPP, true, false)
-        psInformation.addAvp(CALLED_STATION_ID, APN, false)
-        psInformation.addAvp(Avp.GPP_SGSN_MCC_MNC, SGSN_MCC_MNC, VENDOR_ID_3GPP, true, false, true)
+    fun update(ccrAvps: AvpSet, msisdn: String, bucketSize: Long) {
+        build(ccrAvps, RequestType.UPDATE_REQUEST, requestNumber = 1, msisdn = msisdn, bucketSize = bucketSize)
     }
 
-    fun initNoCredit(ccrAvps: AvpSet) {
+    fun terminate(ccrAvps: AvpSet, msisdn: String, bucketSize: Long) {
 
-        ccrAvps.addAvp(Avp.CC_REQUEST_TYPE, RequestType.INITIAL_REQUEST, true, false)
-        ccrAvps.addAvp(Avp.CC_REQUEST_NUMBER, 0, true, false)
+        set(ccrAvps) {
+            avp(Avp.CC_REQUEST_TYPE, RequestType.TERMINATION_REQUEST)
+            avp(Avp.CC_REQUEST_NUMBER, 2)
 
-        val subscriptionId = ccrAvps.addGroupedAvp(Avp.SUBSCRIPTION_ID)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_E164.ordinal)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_DATA, "4333333333", false)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_IMSI.ordinal)
-        subscriptionId.addAvp(Avp.SUBSCRIPTION_ID_DATA, IMSI, false)
+            group(Avp.SUBSCRIPTION_ID) {
+                avp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_E164.ordinal)
+                avp(Avp.SUBSCRIPTION_ID_DATA, msisdn, pFlag = true)
+            }
+            group(Avp.SUBSCRIPTION_ID) {
+                avp(Avp.SUBSCRIPTION_ID_TYPE, SubscriptionType.END_USER_IMSI.ordinal)
+                avp(Avp.SUBSCRIPTION_ID_DATA, IMSI, pFlag = true)
+            }
 
-        ccrAvps.addAvp(Avp.MULTIPLE_SERVICES_INDICATOR, 1)
+            avp(Avp.TERMINATION_CAUSE, 1, pFlag = true) // 1 = DIAMETER_LOGOUT
 
-        val mscc = ccrAvps.addGroupedAvp(Avp.MULTIPLE_SERVICES_CREDIT_CONTROL)
-        mscc.addAvp(Avp.RATING_GROUP, 10)
-        mscc.addAvp(Avp.SERVICE_IDENTIFIER_CCA, 1)
-        val requestedServiceUnits = mscc.addGroupedAvp(Avp.REQUESTED_SERVICE_UNIT)
-        requestedServiceUnits.addAvp(Avp.CC_TOTAL_OCTETS, BUCKET_SIZE)
-        requestedServiceUnits.addAvp(Avp.CC_INPUT_OCTETS, 0L)
-        requestedServiceUnits.addAvp(Avp.CC_OUTPUT_OCTETS, 0L)
+            group(Avp.MULTIPLE_SERVICES_CREDIT_CONTROL) {
+                avp(Avp.RATING_GROUP, 10, pFlag = true)
+                avp(Avp.SERVICE_IDENTIFIER_CCA, 1, pFlag = true)
 
-        val serviceInformation = ccrAvps.addGroupedAvp(Avp.SERVICE_INFORMATION, VENDOR_ID_3GPP, true, false)
-        val psInformation = serviceInformation.addGroupedAvp(Avp.PS_INFORMATION, VENDOR_ID_3GPP, true, false)
-        psInformation.addAvp(CALLED_STATION_ID, APN, false)
-        psInformation.addAvp(Avp.GPP_SGSN_MCC_MNC, SGSN_MCC_MNC, VENDOR_ID_3GPP, true, false, true)
+                group(Avp.REQUESTED_SERVICE_UNIT) {
+                    avp(Avp.CC_TOTAL_OCTETS, bucketSize, pFlag = true)
+                    avp(Avp.CC_INPUT_OCTETS, 0L, pFlag = true)
+                    avp(Avp.CC_OUTPUT_OCTETS, 0L, pFlag = true)
+                    avp(Avp.CC_SERVICE_SPECIFIC_UNITS, 0L, pFlag = true)
+                }
+                avp(Avp.REPORTING_REASON, 2, vendorId = VENDOR_ID_3GPP) // 2 = FINAL
+            }
+
+            group(Avp.SERVICE_INFORMATION, vendorId = VENDOR_ID_3GPP) {
+                group(PS_INFORMATION, vendorId = VENDOR_ID_3GPP) {
+                    avp(CALLED_STATION_ID, APN, pFlag = true)
+                    avp(Avp.GPP_SGSN_MCC_MNC, SGSN_MCC_MNC, vendorId = VENDOR_ID_3GPP, asOctetString = true)
+                }
+            }
+        }
     }
 }
