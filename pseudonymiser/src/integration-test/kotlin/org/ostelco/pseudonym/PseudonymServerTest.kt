620309b457ea5c9720cecde2cc2cb2b99997f45c
Prasanth Ullattil <prasanth@telenordigital.com>
2018-03-23 09:55:49 +0100

    Add integration tests for pseudonymiser

--- pseudonymiser/src/integration-test/kotlin/org/ostelco/pseudonym/PseudonymServerTest.kt
+++ pseudonymiser/src/integration-test/kotlin/org/ostelco/pseudonym/PseudonymServerTest.kt
@@ -14,7 +14,7 @@ import kotlin.test.assertEquals
  */
 class PseudonymServerTest {
 
-    private val msisdn = "MSISDN"
+    private val msisdn = "4790303333"
     companion object {
 
         @JvmField
@@ -28,21 +28,20 @@ class PseudonymServerTest {
     fun testPseudonymServer() {
 
         val response = JerseyClientBuilder().build()
-                ?.target("http://0.0.0.0:${RULE.getLocalPort()}/auth/token")
+                ?.target("http://0.0.0.0:${RULE.getLocalPort()}/pseudonym/current/${msisdn}")
                 ?.request()
-                ?.header("X-MSISDN", msisdn)
                 ?.get()
 
         assertEquals(200, response?.status)
 
-        val customToken = String(response?.readEntity(java.lang.String::class.java)?.toCharArray() ?: CharArray(0)).split(".")
-
-        // println(String(Base64.getDecoder().decode(customToken[0])))
-        val header = JsonParser().parse(String(Base64.getDecoder().decode(customToken[0]))).asJsonObject
-        assertEquals("RS256", header.get("alg").asString)
-
-        // println(String(Base64.getDecoder().decode(customToken[1])))
-        val payload = JsonParser().parse(String(Base64.getDecoder().decode(customToken[1]))).asJsonObject
-        assertEquals(msisdn, payload.get("uid").asString)
+//        val customToken = String(response?.readEntity(java.lang.String::class.java)?.toCharArray() ?: CharArray(0)).split(".")
+//
+//        // println(String(Base64.getDecoder().decode(customToken[0])))
+//        val header = JsonParser().parse(String(Base64.getDecoder().decode(customToken[0]))).asJsonObject
+//        assertEquals("RS256", header.get("alg").asString)
+//
+//        // println(String(Base64.getDecoder().decode(customToken[1])))
+//        val payload = JsonParser().parse(String(Base64.getDecoder().decode(customToken[1]))).asJsonObject
+//        assertEquals(msisdn, payload.get("uid").asString)
     }
 }
