a6c6b45e46d98db371cd91e78ff2bdddbddbb5b6
Bjørn Remseth <rmz@comoyo.com>
2018-04-05 13:20:35 +0200

    Transform table into one that is timestamp and not datime-based.

--- analytic-agents/extractor.sh
+++ analytic-agents/extractor.sh
@@ -13,15 +13,37 @@ SOURCE_DATASET=data_consumption
 SOURCE_TABLE=hourly_consumption
 SOURCE_COORDINATE="${SOURCE_DATASET}.${SOURCE_TABLE}"
 
+
+# The hourly_consumption table contains data where the
+# timestamp-column is a "datime" typed column, not a
+# timestamp typed coumn.  That makes it a little tricky for
+# us to use time series based query mechanisms, so therefore
+# we add a procedure for converting that table into
+# a timestamp-based version.
+CONVERT_TO_TIMESTAMP_CENTRIC_FORMAT="no"
+
+if [ "$CONVERT_TO_TIMESTAMP_CENTRIC_FORMAT" =  "yes" ] ; then
+    TRANSFORMED_INPUT="data_consumption.hourly_consumption_timestamped"
+    bq rm -f "$TRANSFORMED_INPUT"
+    CONVERSION_SQL="select msisdn, bytes, timestamp(timestamp) as tstamp  from data_consumption.hourly_consumption"
+    bq query --destination_table "${TRANSFORMED_INPUT}" "${CONVERSION_SQL}"
+fi
+
+
 # Doing queries of various kinds, internal to the database
 DESTINATION_TABLE="todays_consumption"
 DESTINATION_COORDINATE="${SOURCE_DATASET}.${DESTINATION_TABLE}"
 
 
 bq rm -f "$DESTINATION_COORDINATE"
-# XXX Can't figure out how to interpolate using variable in next l inee
+# # XXX Can't figure out how to interpolate using variable in next l inee
+# read -r -d '' QUERY <<'EOF'
+# 'select * from data_consumption.hourly_consumption'
+# EOF
+
 QUERY='select * from data_consumption.hourly_consumption'
-bq query  --destination_table "$DESTINATION_COORDNATE" "$QUERY" limit 1000
+echo "Query is $QUERY"
+bq query  --destination_table "$DESTINATION_COORDNATE" "$QUERY" limit 30
 
 
 # # Setting up the destination coordinate
