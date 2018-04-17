693164ac4fe7d2a1e229a62324e10f5e4d3ae66e
Bjørn Remseth <rmz@comoyo.com>
2018-04-17 14:12:28 +0200

    Removing now completely defunct code

--- exporter/importer/caseofficer/src/main/java/org/ostelco/caseofficer/ETL.kt
+++
@@ -1,9 +0,0 @@
-package org.ostelco.caseofficer
-
-/**
- * Extract transform and load (ETL) data from the bigquery
- * data-model for consumption and demography and store it in
- * google cloud data store
- *
- * ... tbd
- **/
