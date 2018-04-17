63808cf2c0f3e074d25bbea495f73c18952026d3
Bjørn Remseth <rmz@comoyo.com>
2018-04-17 21:42:52 +0200

    Remove comment that isn't very meaningful any longer.

--- importer/script/wait_for_emulators.sh
+++ importer/script/wait_for_emulators.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-# TODO call start.sh from here. For some reason, it is not working
-# ./start.sh
-
 # Start app
 exec java \
     -Dfile.encoding=UTF-8 \
-    -jar /exporter.jar server /config/config.yaml
+    -jar /importer.jar server /config/config.yaml
 
