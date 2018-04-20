926dbc8a24a2d2860568d24723b0ee6e2f1f43c1
Prasanth Ullattil <prasanth@telenordigital.com>
2018-04-20 09:57:26 +0200

    Clean the exporter script.

--- exporter/script/export_data.sh
+++ exporter/script/export_data.sh
@@ -1,53 +1,66 @@
 #!/bin/bash
+#set -x
 
-exportId=$(uuidgen)
+exportId=$1
+if [ -z "$1" ]; then
+    exportId=$(uuidgen)
+fi
 exportId=${exportId//-}
 exportId=${exportId,,}
-echo "Starting export job for $exportId"
+projectId=pantel-2decb
 
+pseudonymsTable=$projectId.exported_pseudonyms.$exportId
+hourlyConsumptionTable=$projectId.data_consumption.hourly_consumption
+dataConsumptionTable=exported_data_consumption.$exportId
+csvfile=$projectId-dataconsumption-export/$exportId.csv
+
+# Generate the pseudonym tables for this export
+echo "Starting export job for $exportId"
 pseudonymHost="pseudonym-server-service.default.svc.cluster.local"
 startUrl="http://$pseudonymHost/pseudonym/export/$exportId"
 httpStatus=$(curl -sL -w "%{http_code}" $startUrl -o /dev/null)
 
 if [[ $httpStatus != 200 ]]; then
-  echo "Error starting the job $httpStatus"
+  echo "Failed to start the pseudonym table creation: $httpStatus"
   exit
-else
-  echo "Started job $httpStatus"
 fi
 
-
+# Wait for the table creation to finish
 echo "Waiting to finish table export for $exportId"
 sleep 30 &
-
 queryUrl="http://$pseudonymHost/pseudonym/exportstatus/$exportId"
 httpStatus=$(curl -sL -w "%{http_code}" $queryUrl -o /dev/null)
 if [[ $httpStatus != 200 ]]; then
-  echo "Cannot find the job $httpStatus"
+  echo "Failed to query the table creation: $httpStatus"
   exit
 fi
 
 jsonResult=RUNNING
-while [[ $jsonResult = RUNNING ]]; do
+while [[ $jsonResult = RUNNING || $jsonResult = INITIAL ]]; do
   jsonResult=$(curl -X GET $queryUrl  2> /dev/null | sed -n -e 's/.*"status"://p'| cut -d \" -f 2)
 done
+if [[ $jsonResult != FINISHED ]]; then
+  echo "Table creation failed $(curl -X GET $queryUrl  2> /dev/null)"
+  exit
+fi
+echo "Created Table $pseudonymsTable"
 
-echo "Exported table for $exportId"
 
-echo "Creating table pantel-2decb.exported_data_consumption.$exportId"
+echo "Creating table $dataConsumptionTable"
+# SQL for joining pseudonym & hourly consumption tables.
 read -r -d '' sqlForJoin << EOM
 SELECT
    hc.bytes, ps.msisdnid, hc.timestamp
 FROM
-   \`pantel-2decb.data_consumption.hourly_consumption\` as hc
+   \`$hourlyConsumptionTable\` as hc
 JOIN
-  \`pantel-2decb.exported_pseudonyms.$exportId\` as ps
+  \`$pseudonymsTable\` as ps
 ON  ps.msisdn = hc.msisdn
 EOM
-bq --location=EU query --destination_table exported_data_consumption.$exportId --use_legacy_sql=false $sqlForJoin
-echo "Table pantel-2decb.exported_data_consumption.$exportId created."
+# Run the query using bq & dump results to the new table
+bq --location=EU --format=none query --destination_table $dataConsumptionTable --replace --use_legacy_sql=false $sqlForJoin
+echo "Created table $dataConsumptionTable"
 
-echo "Exporting data to csv"
-csvfile=pantel-2decb-dataconsumption-export/$exportId.csv
-bq --location=EU extract --destination_format=CSV exported_data_consumption.$exportId gs://$csvfile
-echo "exported data to gs://$csvfile"
+echo "Exporting data to csv $csvfile"
+bq --location=EU extract --destination_format=CSV $dataConsumptionTable gs://$csvfile
+echo "Exported data to gs://$csvfile"
