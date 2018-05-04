11b18d2894eae99bb25f8f55de9da4fb3881303e
Kjell M. Myksvoll <kmm@comoyo.com>
2018-05-04 10:31:17 +0200

    Adds decoding support for the 'X-Endpoint-API-UserInfo' GC Endpoint HTTP header

--- client-api/src/main/java/org/ostelco/topup/api/resources/SubscriptionResource.java
+++ client-api/src/main/java/org/ostelco/topup/api/resources/SubscriptionResource.java
@@ -1,18 +1,21 @@
 package org.ostelco.topup.api.resources;
 
 import org.ostelco.topup.api.auth.AccessTokenPrincipal;
+import org.ostelco.topup.api.core.EndpointUserInfo;
 import org.ostelco.topup.api.core.Error;
 import org.ostelco.topup.api.core.SubscriptionStatus;
 import org.ostelco.topup.api.db.SubscriberDAO;
 
 import io.dropwizard.auth.Auth;
+import io.vavr.control.Either;
+import javax.validation.Valid;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
-import io.vavr.control.Either;
 
 /**
  * Subscriptions API.
@@ -28,7 +31,8 @@ public class SubscriptionResource extends ResourceHelpers {
     @GET
     @Path("status")
     @Produces({"application/json"})
-    public Response getSubscription(@Auth AccessTokenPrincipal token) {
+    public Response getSubscription(@Auth AccessTokenPrincipal token,
+            @Valid @HeaderParam("X-Endpoint-API-UserInfo") EndpointUserInfo userInfo) {
 
         Either<Error, SubscriptionStatus> result = dao.getSubscriptionStatus(token.getName());
 
