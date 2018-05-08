77508d6e43e20dbb59a9570e0d26ed07f53ebfa8
Martin Cederlof <mape@comoyo.com>
2018-05-08 13:43:15 +0200

    Cloud function for push notifications on low balance

---
+++ cloudfunctions/fcm/functions/index.js
@@ -0,0 +1,38 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp()
+
+/**
+ * Triggers when a user balance get below a threshold and sends a notification.
+ */
+exports.sendUserNotification = functions.database.ref('/fcm-test/{msisdn}')
+    .onCreate(event => {
+
+        const userObject = event.val()
+        const noOfBytes = userObject.noOfBytes
+        const msisdn = userObject.msisdn
+
+        console.log('We have a new balance:', noOfBytes, 'for user:', msisdn);
+
+        // Get the device notification token.
+        admin.database().ref(`/firebaseCloudMessage/${msisdn}`).once('value').then(result => {
+
+            // Notification details.
+            const payload = {
+                notification: {
+                    title: 'You have a low data balance!',
+                    body: `${noOfBytes} bytes left.`
+                }
+            };
+
+            console.log('Sending notification.' + payload);
+            const token = result.val();
+
+            return admin.messaging().sendToDevice(token, payload);
+        }, result => {
+            return console.log('Something failed', result);
+        }).catch(error => { console.log('Error : ', error)});
+    });
