3367547a55cc6715fd5cbfb83a56e6afa5d2c667
Bjørn Remseth <rmz@comoyo.com>
2018-05-16 11:13:40 +0200

    Automatically generating api docs from swagger file.

--- scripts/generate-swagger-docs.sh
+++ scripts/generate-swagger-docs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Generate swagger documentation for the prime API.
+# The output will be placed i the directory  docs/prime-api
+# Anything that happened to be in that directory previously will
+# be overwritten
 
 # Get the basedir of the ostelco core in the
 # local filesystem
@@ -7,13 +11,8 @@ BASEDIR=$($(cd $(dirname "$0")/.. ); pwd)
 
 # Setting up source and destination directories
 SWAGGER_DOC_DIR=${BASEDIR}/docs/prime-api
+SWAGGER_GEN_DIR=${BASEDIR}/.swagger_gen_dir
 SWAGGER_YAML_FILE=${BASEDIR}/prime/infra/prime-api.yaml
-
-
-
-
-
-
 
 rm -rf $SWAGGER_DOC_DIR
 mkdir $SWAGGER_DOC_DIR
@@ -25,3 +24,4 @@ cd $SWAGGER_GEN_DIR
 swagger-codegen generate -i "${SWAGGER_YAML_FILE}" -l swagger
 swagger-gen -d $SWAGGER_DOC_DIR swagger.json
 
+rm -rf ${SWAGGER_GEN_DIR}
