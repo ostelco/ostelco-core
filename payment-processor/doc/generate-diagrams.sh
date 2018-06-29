11f2168a1a0d2e0956a4ffc16ad7df5c64be2d32
Bjørn Remseth <rmz@comoyo.com>
2018-06-29 18:08:21 +0200

    Adding a first stab of a state diagram for payments, the idea being that these states will somehow be tracked in a database, and can be used to guide rollback, should that be needed.

--- payment-processor/doc/generate-diagrams.sh
+++ payment-processor/doc/generate-diagrams.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 plantuml -tsvg -pipe < puml/purchase-flow.puml > diagrams/purchase-flow.svg
+plantuml -tsvg -pipe < puml/purchase-state-diagram.puml > diagrams/purchase-state-diagram.svg
