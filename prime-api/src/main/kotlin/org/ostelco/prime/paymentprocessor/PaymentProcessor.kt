b35bc704f83e11c4bcc3ad17e5357622d7ab02f8
Martin Cederlof <mape@comoyo.com>
2018-07-17 13:52:15 +0200

    PurchaseProduct in PaymentProcessor
    Moved doc to interface

--- prime-api/src/main/kotlin/org/ostelco/prime/paymentprocessor/PaymentProcessor.kt
+++ prime-api/src/main/kotlin/org/ostelco/prime/paymentprocessor/PaymentProcessor.kt
@@ -9,18 +9,57 @@ interface PaymentProcessor {
         YEAR("year")
     }
 
+    /**
+     * @param customerId Stripe customer id
+     * @param sourceId Stripe source id
+     * @return Stripe sourceId or null if not created
+     */
     fun addSource(customerId: String, sourceId: String): String?
 
+    /**
+     * @param userEmail: user email (Prime unique identifier for customer)
+     * @return Stripe customerId or null if not created
+     */
     fun createPaymentProfile(userEmail: String): String?
 
+    /**
+     * @param productId Stripe product id
+     * @param amount The amount to be charged in the interval specified
+     * @param currency Three-letter ISO currency code in lowercase
+     * @param interval The frequency with which a subscription should be billed.
+     * @return Stripe planId or null if not created
+     */
     fun createPlan(productId: String, amount: Int, currency: String, interval: Interval): String?
 
+    /**
+     * @param sku Prime product SKU
+     * @return Stripe productId or null if not created
+     */
     fun createProduct(sku: String): String?
 
+    /**
+     * @param customerId Stripe customer id
+     * @return List of Stripe sourceId or null if none stored
+     */
     fun getSavedSources(customerId: String): List<String>
 
+    /**
+     * @param customerId Stripe customer id
+     * @return Stripe default sourceId or null if not set
+     */
     fun getDefaultSource(customerId: String): String?
 
+    /**
+     * @param customerId Stripe customer id
+     * @return Stripe default chargeId or null failed
+     */
+    fun purchaseProduct(customerId: String, sourceId: String, amount: Int, currency: String): String?
+
+    /**
+     * @param customerId Stripe customer id
+     * @param sourceId Stripe source id
+     * @return Stripe customerId or null if not created
+     */
     fun setDefaultSource(customerId: String, sourceId: String): String?
 
 }
