a2f27b53acfa5d91cbefd3d0422e179f271217af
Martin Cederlof <mape@comoyo.com>
2018-07-17 15:45:29 +0200

    Changed return value to Either

---
+++ prime-api/src/main/kotlin/org/ostelco/prime/paymentprocessor/core/Model.kt
@@ -0,0 +1,9 @@
+package org.ostelco.prime.paymentprocessor.core
+
+class PlanInfo(val id: String)
+
+class ProductInfo(val id: String)
+
+class ProfileInfo(val id: String)
+
+class SourceInfo(val id: String)
