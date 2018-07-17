4243a79e46c777e87102377f62188fa5dee6e8a5
Martin Cederlof <mape@comoyo.com>
2018-07-17 10:56:06 +0200

    Added createPlan for PaymentProcessor

--- payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/StripePaymentProcessor.kt
+++ payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/StripePaymentProcessor.kt
@@ -1,6 +1,7 @@
 package org.ostelco.prime.paymentprocessor
 
 import com.stripe.model.Customer
+import com.stripe.model.Plan
 import org.ostelco.prime.logger
 
 class StripePaymentProcessor : PaymentProcessor {
@@ -24,6 +25,27 @@ class StripePaymentProcessor : PaymentProcessor {
             Customer.create(customerParams)?.id
         } catch (e: Exception) {
             LOG.warn("Failed to create profile", e)
+            null
+        }
+    }
+
+    /**
+     * Return Stripe planId or null if not created
+     */
+    override fun createPlan(productId: String, amount: Int, currency: String, interval: PaymentProcessor.Interval) : String?{
+        val productParams = HashMap<String, Any>()
+        productParams["name"] = "Quartz pro"
+
+        val planParams = HashMap<String, Any>()
+        planParams["amount"] = amount
+        planParams["interval"] = interval.value
+        planParams["product"] = productId
+        planParams["currency"] = currency
+
+        return try {
+            Plan.create(planParams)?.id
+        } catch (e: Exception) {
+            LOG.warn("Failed to create plan", e)
             null
         }
     }
