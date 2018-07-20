7d1ffdb409584f4fff4762149440a706eaf64d9d
Martin Cederlof <mape@comoyo.com>
2018-07-20 12:59:17 +0200

    Updated documentation

--- prime-api/src/main/kotlin/org/ostelco/prime/paymentprocessor/PaymentProcessor.kt
+++ prime-api/src/main/kotlin/org/ostelco/prime/paymentprocessor/PaymentProcessor.kt
@@ -22,7 +22,7 @@ interface PaymentProcessor {
 
     /**
      * @param userEmail: user email (Prime unique identifier for customer)
-     * @return Stripe customerId if not created
+     * @return Stripe customerId if created
      */
     fun createPaymentProfile(userEmail: String): Either<ApiError, ProfileInfo>
 
@@ -31,30 +31,34 @@ interface PaymentProcessor {
      * @param amount The amount to be charged in the interval specified
      * @param currency Three-letter ISO currency code in lowercase
      * @param interval The frequency with which a subscription should be billed.
-     * @return Stripe planId or null if not created
+     * @return Stripe planId if created
      */
     fun createPlan(productId: String, amount: Int, currency: String, interval: Interval): Either<ApiError, PlanInfo>
 
     /**
      * @param sku Prime product SKU
-     * @return Stripe productId or null if not created
+     * @return Stripe productId if created
      */
     fun createProduct(sku: String): Either<ApiError, ProductInfo>
 
     /**
      * @param customerId Stripe customer id
-     * @return List of Stripe sourceId or null if none stored
+     * @return List of Stripe sourceId
      */
     fun getSavedSources(customerId: String): Either<ApiError, List<SourceInfo>>
 
     /**
      * @param customerId Stripe customer id
-     * @return Stripe default sourceId if created
+     * @return Stripe default sourceId
      */
     fun getDefaultSource(customerId: String): Either<ApiError, SourceInfo>
 
     /**
      * @param customerId Stripe customer id
+     * @param sourceId Stripe source id
+     * @param amount The amount to be charged in the interval specified
+     * @param currency Three-letter ISO currency code in lowercase
+     * @param saveSource set if source should be stored with Stripe
      * @return Stripe default chargeId or null failed
      */
     fun purchaseProduct(customerId: String, sourceId: String, amount: Int, currency: String, saveSource: Boolean = true): Either<ApiError, ProductInfo>
