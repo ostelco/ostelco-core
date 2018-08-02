87bba724d5b35a70ce845020384efcd7e7b2012e
Kamran Azeem <kaz@praqma.net>
2018-08-02 11:57:06 +0200

    added --build to docker-compose up. Also added a small secrets distributing script

---
+++ scripts/distribute-secrets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PANTEL_SECRETS_FILE=$(cat /tmp/pantel-prod.json | base64 -w 0)
+
+if [ -z "${PANTEL_SECRETS_FILE}" ] ; then
+  echo "the PANTEL_SECRETS_FILE env var is empty. Can't proceed. Exiting ..."
+  exit 1
+fi
+echo; echo "======> Creating pantel-prod.json file, using the env variable PANTEL_SECRETS_FILE"
+for LOCATION in $(find . -name .gitignore  -exec grep pantel-prod.json  '{}' '+' ); do
+  DIR_NAME=$(dirname $LOCATION)
+  echo "Creating secrets file: ${DIR_NAME}/pantel-prod.json ..."
+  echo ${PANTEL_SECRETS_FILE} | base64 -d >  ${DIR_NAME}/pantel-prod.json
+  ls -l ${DIR_NAME}/pantel-prod.json
+done
+echo ''
+
+echo "Generating SSL certs ..."
+cd certs/ocs.ostelco.org/
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx.key -out ./nginx.crt -subj '/CN=ocs.ostelco.org'
+cp nginx.crt ../../ocsgw/config
+cd -
+
