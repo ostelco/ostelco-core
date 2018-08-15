fface17765c1dbf8130d9cb53cee38b005823d17
Vihang Patil <vihang.patil@gmail.com>
2018-08-15 13:23:50 +0200

    Passing STRIPE_API_KEY to prime from ENV instead of reading from file.

--- payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/PaymentProcessorModule.kt
+++ payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/PaymentProcessorModule.kt
@@ -1,31 +1,18 @@
 package org.ostelco.prime.paymentprocessor
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeName
-import io.dropwizard.setup.Environment
-import org.hibernate.validator.constraints.NotEmpty
-import org.ostelco.prime.module.PrimeModule
-import java.io.File
 import com.stripe.Stripe
+import io.dropwizard.setup.Environment
+import org.ostelco.prime.logger
+import org.ostelco.prime.module.PrimeModule
 
 @JsonTypeName("stripe-payment-processor")
 class PaymentProcessorModule : PrimeModule {
 
-
-    @JsonProperty("config")
-    fun setConfig(config: PaymentProcessorConfig) {
-        val secretKey = File(config.configFile).readText(Charsets.UTF_8)
-        Stripe.apiKey = secretKey
-    }
+    private val logger by logger()
 
     override fun init(env: Environment) {
-        println("PaymentProcessor init with ${env}")
+        logger.info("PaymentProcessor init with $env")
+        Stripe.apiKey = System.getenv("STRIPE_API_KEY") ?: throw Error("Missing environment variable STRIPE_API_KEY")
     }
-}
-
-class PaymentProcessorConfig {
-
-    @NotEmpty
-    @JsonProperty("configFile")
-    lateinit var configFile: String
 }
