3c69bdcdf5abe0bcf628ec034dd411dbbd5e042f
Kjell M. Myksvoll <kmm@comoyo.com>
2018-08-24 13:49:11 +0200

    Updates to use the dropwizards gauge metric for active sessions

--- analytics-module/src/main/kotlin/org/ostelco/prime/analytics/metrics/OcsgwMetrics.kt
+++ analytics-module/src/main/kotlin/org/ostelco/prime/analytics/metrics/OcsgwMetrics.kt
@@ -1,6 +1,6 @@
 package org.ostelco.prime.analytics.metrics
 
-import com.codahale.metrics.Counter
+import com.codahale.metrics.Gauge
 import com.codahale.metrics.MetricRegistry
 import com.codahale.metrics.MetricRegistry.name
 import org.ostelco.prime.analytics.AnalyticsGrpcService
@@ -8,11 +8,17 @@ import org.ostelco.prime.analytics.AnalyticsGrpcService
 
 class OcsgwMetrics(private val registry: MetricRegistry) {
 
-    private val activeSessions : Counter
+    private val activeSessions: Gauge<Long>
+
+    private val currentActiveSessions = object {
+        var timestamp: Long = 0
+        var count: Long = 0
+    }
 
     init {
-        this.activeSessions = registry.counter(name(AnalyticsGrpcService::class.java,
-                "active-sessions"))
+        this.activeSessions = registry.register(
+                name(AnalyticsGrpcService::class.java, "active-sessions"),
+                Gauge<Long> { currentActiveSessions.count })
     }
 
     /**
@@ -20,12 +26,9 @@ class OcsgwMetrics(private val registry: MetricRegistry) {
      * @param timestamp - seconds since epoch
      * @param count - active sessions
      */
-    @Synchronized fun setActiveSessions(timestamp: Long, count: Long) {
-        val currentCount = activeSessions.getCount()
-        if (currentCount < count) {
-            activeSessions.inc(count - currentCount)
-        } else if (currentCount > count) {
-            activeSessions.dec(currentCount - count)
-        }
+    @Synchronized
+    fun setActiveSessions(timestamp: Long, count: Long) {
+        currentActiveSessions.timestamp = timestamp
+        currentActiveSessions.count = count
     }
 }
