2f65e0ba7158d36c13d12d7bc959954b8f4fca09
Bjørn Remseth <rmz@comoyo.com>
2018-09-07 15:08:00 +0200

    We're now reading one metric in the grafana (dev), declaring a bit of success.

--- bq-metrics-extractor/script/start.sh
+++ bq-metrics-extractor/script/start.sh
@@ -3,4 +3,4 @@
 # Start app
 exec java \
     -Dfile.encoding=UTF-8 \
-    -jar /bq-metrics-extractor.jar query --pushgateway pushgateway:9091 config/config.yaml
+    -jar /bq-metrics-extractor.jar query --pushgateway pushgateway:8080 config/config.yaml
