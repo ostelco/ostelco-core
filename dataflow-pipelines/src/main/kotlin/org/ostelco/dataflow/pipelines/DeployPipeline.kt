5c649a7602b33473b645be55e1db6ec9a0cfc333
Prasanth Ullattil <prasanth@telenordigital.com>
2018-10-04 10:39:57 +0200

    Move to commandline options
    - Added 2 new options
    - Accepts commandline start options.
    - Different files for dev and prod

--- dataflow-pipelines/src/main/kotlin/org/ostelco/dataflow/pipelines/DeployPipeline.kt
+++ dataflow-pipelines/src/main/kotlin/org/ostelco/dataflow/pipelines/DeployPipeline.kt
@@ -1,61 +1,61 @@
 package org.ostelco.dataflow.pipelines
 
 import ch.qos.logback.classic.util.ContextInitializer
-import org.apache.beam.runners.dataflow.DataflowRunner
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions
 import org.apache.beam.sdk.Pipeline
-import org.apache.beam.sdk.options.PipelineOptions
+import org.apache.beam.sdk.options.Default
+import org.apache.beam.sdk.options.Description
 import org.apache.beam.sdk.options.PipelineOptionsFactory
 import org.ostelco.dataflow.pipelines.definitions.DataConsumptionPipelineDefinition
 import org.ostelco.dataflow.pipelines.definitions.DummyPipelineDefinition
 import org.ostelco.dataflow.pipelines.definitions.PipelineDefinition
+
 
 enum class PipelineDefinitionRegistry(val pipelineDefinition: PipelineDefinition) {
     DATA_CONSUMPTION(DataConsumptionPipelineDefinition),
     DUMMY(DummyPipelineDefinition),
 }
 
+interface ConsumptionPipelineOptions : DataflowPipelineOptions {
+    @get:Description("Dataset name.")
+    @get:Default.String("data_consumption")
+    var dataset: String
+    @get:Description("PubSub toipc name.")
+    @get:Default.String("data-traffic")
+    var pubsubTopic: String
+}
+
 fun main(args: Array<String>) {
     System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, "config/logback.xml")
-    DeployPipeline().deploy(pipelineName = "DATA_CONSUMPTION")
+    DeployPipeline().deploy(
+            pipelineName = "DATA_CONSUMPTION",
+            args = args)
 }
 
 class DeployPipeline {
 
-    private fun parseOptions(): PipelineOptions {
+    private fun parseOptions(args: Array<String>): ConsumptionPipelineOptions {
 
-        // may be we need to pass options via command-line args
-        /*
-    val options = PipelineOptionsFactory
-            .fromArgs(
-                    "--project=pantel-2decb",
-                    "--runner=DataflowRunner",
-                    "--stagingLocation=gs://data-traffic/staging/",
-                    "--jobName=data-traffic")
-            .withValidation()
-            .create()
-    */
+        PipelineOptionsFactory.register(ConsumptionPipelineOptions::class.java)
 
-        val options = PipelineOptionsFactory.`as`(DataflowPipelineOptions::class.java)
-        options.jobName = "data-traffic"
-        options.project = "pantel-2decb"
-        options.stagingLocation = "gs://data-traffic/staging/"
-        options.region = "europe-west1"
-        options.runner = DataflowRunner::class.java
-        options.isUpdate = true
+        val options = PipelineOptionsFactory
+                .fromArgs(*args)
+                .withValidation()
+                .`as`(ConsumptionPipelineOptions::class.java)
+
+        println("${options.dataset}, ${options.pubsubTopic}, ${options.jobName}, ${options.isUpdate} ")
 
         return options
     }
 
-    fun deploy(pipelineName: String) {
+    fun deploy(pipelineName: String, args: Array<String>) {
 
-        val options = parseOptions()
-
+        val options = parseOptions(args)
         PipelineDefinitionRegistry
                 .valueOf(pipelineName)
                 .apply {
                     Pipeline.create(options)
-                            .apply { pipelineDefinition.define(this) }
+                            .apply { pipelineDefinition.define(this, options) }
                             .run()
                             .waitUntilFinish()
                 }
