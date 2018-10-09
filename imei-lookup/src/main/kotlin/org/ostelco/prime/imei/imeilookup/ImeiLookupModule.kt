9d4679c985413e47af5071a603101a8163d2a3e6
mpeterss <mape@comoyo.com>
2018-10-09 15:01:07 +0200

    Move csv file

--- imei-lookup/src/main/kotlin/org/ostelco/prime/imei/imeilookup/ImeiLookupModule.kt
+++ imei-lookup/src/main/kotlin/org/ostelco/prime/imei/imeilookup/ImeiLookupModule.kt
@@ -3,27 +3,21 @@ package org.ostelco.prime.imei.imeilookup
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeName
 import io.dropwizard.setup.Environment
-import org.ostelco.prime.getLogger
 import org.ostelco.prime.module.PrimeModule
 
 
 @JsonTypeName("Imei-lookup")
 class ImeiLookupModule : PrimeModule {
 
-    private val logger by getLogger()
-
     @JsonProperty
-    var config: Config? = null
+    lateinit var config: Config
 
     override fun init(env: Environment) {
-
-        val fileName = config?.csvFile  ?: ""
-        logger.info("CSV file set to $fileName")
-        ImeiDb.ImeiDdSingleton.loadFile(fileName);
+        ImeiDdSingleton.loadFile(config.csvFile);
     }
 }
 
 class Config {
     @JsonProperty
-    var csvFile: String = "default.txt"
+    lateinit var csvFile: String
 }
