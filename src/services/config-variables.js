6473b789c98c4f8966c4302b545f7bbd420c4180
Prasanth Ullattil <prasanth@telenordigital.com>
2018-10-12 16:33:38 +0200

    Whitespace fixes

--- src/services/config-variables.js
+++ src/services/config-variables.js
@@ -20,7 +20,7 @@ export function getAuthConfig() {
   }
 }
 
-export function getAPIRoot(){
+export function getAPIRoot() {
   const API_ROOT = 'https://houston-api.dev.ostelco.org/';
   if (process.env.REACT_APP_DEPLOYMENT_ENV === "development") {
     return API_ROOT;
