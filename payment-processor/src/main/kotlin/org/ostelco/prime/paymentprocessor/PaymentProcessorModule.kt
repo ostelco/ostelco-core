5d8cb66e853c858e1be0f404f95a01875ad19539
Kjell M. Myksvoll <kmm@comoyo.com>
2018-10-22 14:19:12 +0200

    Adds webhook service for Stripe events (WIP)

--- payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/PaymentProcessorModule.kt
+++ payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/PaymentProcessorModule.kt
@@ -5,6 +5,7 @@ import com.stripe.Stripe
 import io.dropwizard.setup.Environment
 import org.ostelco.prime.getLogger
 import org.ostelco.prime.module.PrimeModule
+import org.ostelco.prime.paymentprocessor.resources.StripeWebhookResource
 
 @JsonTypeName("stripe-payment-processor")
 class PaymentProcessorModule : PrimeModule {
@@ -14,5 +15,11 @@ class PaymentProcessorModule : PrimeModule {
     override fun init(env: Environment) {
         logger.info("PaymentProcessor init with $env")
         Stripe.apiKey = System.getenv("STRIPE_API_KEY") ?: throw Error("Missing environment variable STRIPE_API_KEY")
+
+        val jerseyEnv = env.jersey()
+
+        /* APIs. */
+        jerseyEnv.register(StripeWebhookResource())
     }
+
 }
---
+++ payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/resources/StripeWebhookResource.kt
@@ -0,0 +1,51 @@
+package org.ostelco.prime.paymentprocessor.resources
+
+import com.google.gson.JsonSyntaxException
+import com.stripe.Stripe
+import com.stripe.exception.SignatureVerificationException
+import com.stripe.model.Event
+import com.stripe.net.Webhook
+import org.ostelco.prime.getLogger
+import org.ostelco.prime.notifications.NOTIFY_OPS_MARKER
+import javax.validation.Valid
+import javax.validation.constraints.NotNull
+import javax.ws.rs.HeaderParam
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.Response
+
+/**
+ * Webhook service for handling Stripe event reports.
+ *
+ */
+@Path("/stripe/event")
+class StripeWebhookResource {
+
+    private val logger by getLogger()
+
+    @POST
+    @Produces("application/json")
+    fun handleEvent(@NotNull @Valid @HeaderParam("Stripe-Signature")
+                    signature: String,
+                    @NotNull @Valid
+                    payload: String): Response {
+
+        val event: Event = try {
+            Webhook.constructEvent(payload, signature, Stripe.apiKey)
+        } catch (e: JsonSyntaxException) {
+            logger.error("Invalid payload in Stripe event ${e}")
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .build()
+        } catch (e: SignatureVerificationException) {
+            logger.error("Invalid signature for Stipe event ${e}")
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .build()
+        }
+
+        logger.info(NOTIFY_OPS_MARKER, "Got Stripe event ${event.type}")
+
+        return Response.status(Response.Status.OK)
+                .build()
+    }
+}
