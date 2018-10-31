0e887814f2df056b057ad3870929a5ca2ecaaeec
Kjell M. Myksvoll <kmm@comoyo.com>
2018-10-31 13:04:06 +0100

    Updates formatting of Stripe event logs

--- payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/StripeEventReporter.kt
+++ payment-processor/src/main/kotlin/org/ostelco/prime/paymentprocessor/StripeEventReporter.kt
@@ -3,6 +3,10 @@ package org.ostelco.prime.paymentprocessor
 import com.stripe.model.*
 import org.ostelco.prime.getLogger
 import org.ostelco.prime.notifications.NOTIFY_OPS_MARKER
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class StripeEventReporter {
 
@@ -25,7 +29,8 @@ class StripeEventReporter {
             is Source -> report(event, data)
             is Subscription -> report(event, data)
             else -> {
-                logger.error(NOTIFY_OPS_MARKER, "No handler found for Stripe event ${event.type}")
+                logger.error(NOTIFY_OPS_MARKER, format("No handler found for Stripe event ${event.type}",
+                        event))
             }
         }
     }
@@ -33,141 +38,145 @@ class StripeEventReporter {
     private fun report(event: Event, balance: Balance) {
         when {
             event.type == "balance.available" -> logger.info(NOTIFY_OPS_MARKER,
-                    "Your balance has new available transactions" +
+                    format("Your balance has new available transactions" +
                             "${currency(balance.available[0].amount, balance.available[0].currency)} is available, " +
-                            "${currency(balance.pending[0].amount, balance.pending[0].currency)} is pending." +
-                            url(event)
+                            "${currency(balance.pending[0].amount, balance.pending[0].currency)} is pending.",
+                            event)
             )
             else -> logger.error(NOTIFY_OPS_MARKER,
-                    "Unhandled Stripe event ${event.type} (cat: Balance) " +
-                            url(event))
+                    format("Unhandled Stripe event ${event.type} (cat: Balance)",
+                            event))
         }
     }
 
     private fun report(event: Event, card: Card) {
         when {
             event.type == "customer.source.created" -> logger.info(NOTIFY_OPS_MARKER,
-                    "${email(card.customer)} added a new ${card.brand} ending in ${card.last4} " +
-                            url(event)
+                    format("${email(card.customer)} added a new ${card.brand} ending in ${card.last4}",
+                            event)
             )
             event.type == "customer.source.deleted" -> logger.info(NOTIFY_OPS_MARKER,
-                    "${email(card.customer)} deleted a ${card.brand} ending in ${card.last4} " +
-                            url(event)
+                    format("${email(card.customer)} deleted a ${card.brand} ending in ${card.last4}",
+                            event)
             )
             else -> logger.error(NOTIFY_OPS_MARKER,
-                    "Unhandled Stripe event ${event.type} (cat: Card) " +
-                            url(event))
+                    format("Unhandled Stripe event ${event.type} (cat: Card)",
+                            event))
         }
     }
 
     private fun report(event: Event, charge: Charge) {
         when {
             event.type == "charge.captured" -> logger.info(NOTIFY_OPS_MARKER,
-                    "${email(charge.customer)}'s payment was captured for ${currency(charge.amount, charge.currency)} " +
-                            url(event)
+                    format("${email(charge.customer)}'s payment was captured for ${currency(charge.amount, charge.currency)}",
+                            event)
             )
             event.type == "charge.succeeded" -> logger.info(NOTIFY_OPS_MARKER,
-                    "An uncaptured payment for ${currency(charge.amount, charge.currency)} was created for ${email(charge.customer)} " +
-                            url(event)
+                    format("An uncaptured payment for ${currency(charge.amount, charge.currency)} was created for ${email(charge.customer)}",
+                            event)
             )
             event.type == "charge.refunded" -> logger.info(NOTIFY_OPS_MARKER,
-                    "A ${currency(charge.amount, charge.currency)} payment was refunded to ${email(charge.customer)} " +
-                            url(event)
+                    format("A ${currency(charge.amount, charge.currency)} payment was refunded to ${email(charge.customer)}",
+                            event)
             )
             else -> logger.error(NOTIFY_OPS_MARKER,
-                    "Unhandled Stripe event ${event.type} (cat: Charge) " +
-                            url(event))
+                    format("Unhandled Stripe event ${event.type} (cat: Charge)",
+                            event))
         }
     }
 
     private fun report(event: Event, customer: Customer) {
         when {
             event.type == "customer.created" -> logger.info(NOTIFY_OPS_MARKER,
-                    "${customer.email} is a new customer " +
-                            url(event)
+                    format("${customer.email} is a new customer",
+                            event)
             )
             event.type == "customer.deleted" -> logger.info(NOTIFY_OPS_MARKER,
-                    "${customer.email} had been deleted " +
-                            url(event)
+                    format("${customer.email} had been deleted",
+                            event)
             )
             event.type == "customer.updated" -> logger.info(NOTIFY_OPS_MARKER,
-                    "${customer.email}'s details where updated " +
-                            url(event)
+                    format("${customer.email}'s details where updated",
+                            event)
             )
             else -> logger.error(NOTIFY_OPS_MARKER,
-                    "Unhandled Stripe event ${event.type} (cat: Customer) " +
-                            url(event))
+                    format("Unhandled Stripe event ${event.type} (cat: Customer)",
+                            event))
         }
     }
 
     private fun report(event: Event, dispute: Dispute) {
         logger.error(NOTIFY_OPS_MARKER,
-                "Unhandled Stripe event ${event.type} (cat: Dispute) " +
-                        url(event))
+                format("Unhandled Stripe event ${event.type} (cat: Dispute)",
+                        event))
     }
 
     private fun report(event: Event, payout: Payout) {
         when {
             event.type == "payout.created" -> logger.info(NOTIFY_OPS_MARKER,
-                    "A new payout for ${currency(payout.amount, payout.currency)} was created and will be deposited " +
-                            "on ${epochToDate(payout.arrivalDate)}" +
-                            url(event)
+                    format("A new payout for ${currency(payout.amount, payout.currency)} was created and will be deposited " +
+                            "on ${millisToDate(payout.arrivalDate)}",
+                            event)
             )
             event.type == "payout.paid" -> logger.info(NOTIFY_OPS_MARKER,
-                    "A payout of ${currency(payout.amount, payout.currency)} should now appear on your bank account statement " +
-                            url(event)
+                    format("A payout of ${currency(payout.amount, payout.currency)} should now appear on your bank account statement",
+                            event)
             )
             else -> logger.error(NOTIFY_OPS_MARKER,
-                    "Unhandled Stripe event ${event.type} (cat: Payout) " +
-                            url(event))
+                    format("Unhandled Stripe event ${event.type} (cat: Payout)",
+                            event))
         }
     }
 
     private fun report(event: Event, plan: Plan) {
         logger.error(NOTIFY_OPS_MARKER,
-                "Unhandled Stripe event ${event.type} (cat: Plan) " +
-                        url(event))
+                format("Unhandled Stripe event ${event.type} (cat: Plan)",
+                        event))
     }
 
     private fun report(event: Event, product: Product) {
         logger.error(NOTIFY_OPS_MARKER,
-                "Unhandled Stripe event ${event.type} (cat: Product) " +
-                        url(event))
+                format("Unhandled Stripe event ${event.type} (cat: Product)",
+                        event))
     }
 
     private fun report(event: Event, refund: Refund) {
         logger.error(NOTIFY_OPS_MARKER,
-                "Unhandled Stripe event ${event.type} (cat: Refund) " +
-                        url(event))
+                format("Unhandled Stripe event ${event.type} (cat: Refund)",
+                        event))
     }
 
     private fun report(event: Event, source: Source) {
         when {
             event.type == "customer.source.created" -> logger.info(NOTIFY_OPS_MARKER,
-                    "${email(source.customer)} added a new payment source " +
-                            url(event)
+                    format("${email(source.customer)} added a new payment source",
+                            event)
             )
             event.type == "customer.source.deleted" -> logger.info(NOTIFY_OPS_MARKER,
-                    "Customer ${email(source.customer)} deleted a payment source " +
-                            url(event)
+                    format("Customer ${email(source.customer)} deleted a payment source",
+                            event)
             )
             event.type == "source.chargeable" -> logger.info(NOTIFY_OPS_MARKER,
-                    "A source with ID ${source.id} is chargeable " +
-                            url(event)
+                    format("A source with ID ${source.id} is chargeable",
+                            event)
             )
             else -> logger.error(NOTIFY_OPS_MARKER,
                     "Unhandled Stripe event ${event.type} (cat: Source)" +
-                            url(event))
+                            url(event.id))
         }
     }
 
     private fun report(event: Event, subscription: Subscription) {
-        logger.error("Unhandled Stripe event ${event.type} (cat: Subscription) " +
-                url(event))
+        logger.error(format("Unhandled Stripe event ${event.type} (cat: Subscription)",
+                event))
     }
 
-    private fun url(event: Event): String {
-        return "https://dashboard.stripe.com/events/${event.id}"
+    private fun format(s: String, event: Event): String {
+        return "${s} ${url(event.id)}"
+    }
+
+    private fun url(eventId: String): String {
+        return "https://dashboard.stripe.com/events/${eventId}"
     }
 
     private fun currency(amount: Long, currency: String): String {
@@ -182,8 +191,11 @@ class StripeEventReporter {
         return if (email.isNullOrEmpty()) "****" else email
     }
 
-    // TODO: Fix this!
-    private fun epochToDate(ts: Long): String {
-        return "YYYY/MM/DD"
+    /* Convert millis since epoch to the date in YYYY-MM-DD format.
+       Assumes millis to be UTC. */
+    private fun millisToDate(ts: Long): String {
+        val utc = LocalDateTime.ofInstant(Instant.ofEpochMilli(ts),
+                ZoneOffset.UTC)
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(utc)
     }
 }
