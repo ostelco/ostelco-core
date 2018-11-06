02a550bc23568145969f8972a7305664fecea07a
Bjørn Remseth <rmz@comoyo.com>
2018-11-06 17:10:02 +0100

    Now validating DownloadOrder in test setup

--- esim/es2/esim/src/test/java/org/ostelco/TestJsonValidation.kt
+++ esim/es2/esim/src/test/java/org/ostelco/TestJsonValidation.kt
@@ -9,11 +9,22 @@ import org.junit.Test
 class TestJsonValidation() {
 
     @Test
-    fun testJsonValidation() {
+    fun helloWorldTest() {
         val inputStream = this.javaClass.getResourceAsStream("/hello-world-schema.json")
         assertNotNull(inputStream)
         val rawSchema = JSONObject(JSONTokener(inputStream))
         val schema =  org.everit.json.schema.loader.SchemaLoader.load(rawSchema)
         schema.validate(JSONObject("{\"hello\" : \"world\"}"))
+    }
+
+
+
+    @Test
+    fun ES2DownloadOrderTest() {
+        val inputStream = this.javaClass.getResourceAsStream("/es2schemas/ES2+DownloadOrder-def.json")
+        assertNotNull(inputStream)
+        val rawSchema = JSONObject(JSONTokener(inputStream))
+        val schema =  org.everit.json.schema.loader.SchemaLoader.load(rawSchema)
+        schema.validate(JSONObject( "{\"eid\" : \"01234567890123456789012345678901\", \"iccid\" : \"01234567890123456789\", \"profileType\" : \"Eplestang\"}"))
     }
 }
