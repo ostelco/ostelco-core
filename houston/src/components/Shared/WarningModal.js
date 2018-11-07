b8a4cd430d904a21b520b899b47869cd95c01722
Prasanth Ullattil <prasanth@telenordigital.com>
2018-11-07 18:10:00 +0100

    Warning fixes

--- houston/src/components/Shared/WarningModal.js
+++ houston/src/components/Shared/WarningModal.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal, Button, Popover, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 
 const WarningModal = props => {
-  const popover = (
-    <Popover id="modal-popover" title="popover">
-      very popover. such engagement
-    </Popover>
-  );
-  const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
   return (
     <Modal show={props.show} onHide={props.handleClose}>
-    <Modal.Header closeButton>
-      <Modal.Title>{props.heading}</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <p>
-        {props.warningText}
-      </p>
-
-    </Modal.Body>
-    <Modal.Footer>
-      <Button onClick={props.handleClose}>Close</Button>
-      <Button onClick={props.handleConfirm} bsStyle="primary">Yes</Button>
-    </Modal.Footer>
-  </Modal>
+      <Modal.Header closeButton>
+        <Modal.Title>{props.heading}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          {props.warningText}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.handleClose}>Close</Button>
+        <Button onClick={props.handleConfirm} bsStyle="primary">Yes</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
