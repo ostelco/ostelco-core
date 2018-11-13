eb73910daa5bf169d459b44ab2bfbc910d899cdb
Bjørn Remseth <la3lma@gmail.com>
2018-11-13 16:36:04 +0100

    Removing more cruft, updating openapi-generating script

--- sim-inventory/update-openapi-spec.sh
+++ sim-inventory/update-openapi-spec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DESTINATION=resources/openapi-es2-spec.yaml
+DESTINATION=resources/openai-sim-inventory--spec.yaml
 
 curl --header "User-Agent: gsma-rsp-lpad" --header "X-Admin-Protocol: gsma/rsp/foo.bar" http://localhost:8080/openapi.yaml > $DESTINATION
 
