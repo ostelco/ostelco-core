9be8fe1a854080e64d5aa81120d7d830f8635775
Bjørn Remseth <la3lma@gmail.com>
2018-11-14 22:07:31 +0100

    Comments

--- sim-inventory/src/main/java/org/ostelco/SimAdministrationApplication.kt
+++ sim-inventory/src/main/java/org/ostelco/SimAdministrationApplication.kt
@@ -321,7 +321,8 @@ class SimImportBatchReader(val csvInputStream: InputStream) {
         // XXX Adjust to fit whatever format we should cater to, there may
         //     be some variation between  sim vendors, and that should be
         //     something we can adjust to given the parameters sent to the
-        //     reader class on creation.
+        //     reader class on creation.   Should anyway be configurable in
+        //     a config file or perhaps some config database.
         val csvFileFormat = CSVFormat.DEFAULT
                 .withQuote(null)
                 .withIgnoreEmptyLines(true)
