7f9c699d20158f0b8931d9b08bc2096dcb8b0e7e
Prasanth Ullattil <prasanth@telenordigital.com>
2018-11-20 07:14:38 +0100

    Add tests for rendering App

--- houston/src/components/App.test.js
+++ houston/src/components/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store } from '../helpers';
 import App from './App';
 
+export default function TestApp(props) {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<TestApp />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
