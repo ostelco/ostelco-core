fc8a1d4f884a2c52a2812f453f5230c1b4311cab
Prasanth Ullattil <prasanth@telenordigital.com>
2018-11-20 08:36:03 +0100

    Add dependencies for shallow rendering tests

--- houston/src/components/__tests__/App.js
+++ houston/src/components/__tests__/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { store } from '../helpers';
-import App from './App';
+import { store } from '../../helpers';
+import App from '../App';
 
 export default function TestApp(props) {
   return (
