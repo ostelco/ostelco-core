9bd6035da9cc29f6ce1f53b7c9bbd7fe9e06e359
Bjørn Remseth <la3lma@gmail.com>
2018-11-22 15:58:44 +0100

    Remove es2plus (old) directory, since it is now all movesim-inventory

--- es2plus/update-openapi-spec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DESTINATION=resources/openapi-es2-spec.yaml
-
-curl --header "User-Agent: gsma-rsp-lpad" --header "X-Admin-Protocol: gsma/rsp/foo.bar" http://localhost:8080/openapi.yaml > $DESTINATION
-
-
