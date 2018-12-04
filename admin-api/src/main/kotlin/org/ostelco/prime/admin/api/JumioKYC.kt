a962ec610598abf7ad1c8a39414c36223c52eef8
Prasanth Ullattil <prasanth@telenordigital.com>
2018-12-04 17:30:29 +0100

    Add data to logs

--- admin-api/src/main/kotlin/org/ostelco/prime/admin/api/JumioKYC.kt
+++ admin-api/src/main/kotlin/org/ostelco/prime/admin/api/JumioKYC.kt
@@ -21,13 +21,15 @@ class KYCResource {
     @Produces(MediaType.APPLICATION_JSON)
     fun handleCallback(
             @Context request: HttpServletRequest,
-            @Context httpHeaders: HttpHeaders): Response {
-        return Response.status(Response.Status.OK).entity(printHeaderInfo(request, httpHeaders)).build();
+            @Context httpHeaders: HttpHeaders,
+            data: String): Response {
+        return Response.status(Response.Status.OK).entity(printHeaderInfo(request, httpHeaders, data)).build();
     }
-    private fun printHeaderInfo(request: HttpServletRequest, httpHeaders: HttpHeaders):String {
+    private fun printHeaderInfo(request: HttpServletRequest, httpHeaders: HttpHeaders, data: String):String {
         var result = ""
         result += "Address: ${request.remoteHost} (${request.remoteAddr} : ${request.remotePort}) \n"
         result += "Query: ${request.queryString} \n"
+        result += "Data: ${data} \n"
         result += "Headers == > \n"
         val requestHeaders = httpHeaders.getRequestHeaders()
         for (entry in requestHeaders.entries) {
