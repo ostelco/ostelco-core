0fdd999cc2d0ff37d175dbc2e6f167a4fd913d66
Prasanth Ullattil <prasanth@telenordigital.com>
2018-12-04 17:13:20 +0100

    Add query string

--- admin-api/src/main/kotlin/org/ostelco/prime/admin/api/JumioKYC.kt
+++ admin-api/src/main/kotlin/org/ostelco/prime/admin/api/JumioKYC.kt
@@ -26,8 +26,9 @@ class KYCResource {
     }
     private fun printHeaderInfo(request: HttpServletRequest, httpHeaders: HttpHeaders):String {
         var result = ""
-        result += request.remoteHost + " (" + request.remoteAddr + ":" + request.remotePort + ")"
-
+        result += "Address: ${request.remoteHost} (${request.remoteAddr} : ${request.remotePort}) \n"
+        result += "Query: ${request.queryString} \n"
+        result += "Headers == > \n"
         val requestHeaders = httpHeaders.getRequestHeaders()
         for (entry in requestHeaders.entries) {
             result += "${entry.key} = ${entry.value}\n"
