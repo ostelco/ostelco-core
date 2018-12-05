42d7920f8056de63489b355a541d114fb810ed62
Prasanth Ullattil <prasanth@telenordigital.com>
2018-12-05 08:54:02 +0100

    Use map instead of string for 'form data'

--- admin-api/src/main/kotlin/org/ostelco/prime/admin/api/KYCResource.kt
+++ admin-api/src/main/kotlin/org/ostelco/prime/admin/api/KYCResource.kt
@@ -5,10 +5,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
-import javax.ws.rs.core.Context
-import javax.ws.rs.core.HttpHeaders
-import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
+import javax.ws.rs.core.*
 
 /**
  * Resource used to handle the eKYC related REST calls.
@@ -22,17 +19,20 @@ class KYCResource {
     fun handleCallback(
             @Context request: HttpServletRequest,
             @Context httpHeaders: HttpHeaders,
-            data: String): Response {
-        return Response.status(Response.Status.OK).entity(printHeaderInfo(request, httpHeaders, data)).build();
+            formData: MultivaluedMap<String, String>): Response {
+        return Response.status(Response.Status.OK).entity(getRequestInfo(request, httpHeaders, formData)).build();
     }
-    private fun printHeaderInfo(request: HttpServletRequest, httpHeaders: HttpHeaders, data: String):String {
+    private fun getRequestInfo(request: HttpServletRequest, httpHeaders: HttpHeaders, formData: MultivaluedMap<String, String>):String {
         var result = ""
         result += "Address: ${request.remoteHost} (${request.remoteAddr} : ${request.remotePort}) \n"
         result += "Query: ${request.queryString} \n"
-        result += "Data: ${data} \n"
         result += "Headers == > \n"
         val requestHeaders = httpHeaders.getRequestHeaders()
         for (entry in requestHeaders.entries) {
+            result += "${entry.key} = ${entry.value}\n"
+        }
+        result += "Data: \n"
+        for (entry in formData.entries) {
             result += "${entry.key} = ${entry.value}\n"
         }
         logger.info("$result")
