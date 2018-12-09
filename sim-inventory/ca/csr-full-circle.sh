ce14ce8947fc0c0d8f9472412781b27d26b67b1c
Bjørn Remseth <la3lma@gmail.com>
2018-12-09 16:49:48 +0100

    add comments about what to do next

--- sim-inventory/ca/csr-full-circle.sh
+++ sim-inventory/ca/csr-full-circle.sh
@@ -24,7 +24,7 @@ openssl genrsa -out $REQUESTER_KEY 2048
 
 # Generate a CSR using configuration from the oats.conf
 # file (should later be parameterized)
-openssl req -new -out oats.csr -config $REQUESTER_CONF
+openssl req -new -out $REQUEST_CSR -config $REQUESTER_CONF
 
 ##
 ## Creating certificate authority (CA), sign the CSR, and
@@ -35,6 +35,7 @@ openssl req -new -out oats.csr -config $REQUESTER_CONF
 # The domain of the CA
 CA_DOMAIN=ca
 CA_KEY="${CA_DOMAIN}.key"
+CA_PUBKEY="${CA_DOMAIN}.pubkey"
 CA_CRT="${CA_DOMAIN}.crt"
 CA_SERIAL_NUMBER_FILE=${CA_DOMAIN}.srl
 CA_CONF=ca.conf
@@ -42,9 +43,20 @@ CA_CONF=ca.conf
 # Generate a secret ckey for the CA
 openssl genrsa -out $CA_KEY  2048
 
+# Then extract the public part of the CA key
+openssl rsa -in $CA_KEY -pubout -out $CA_PUBKEY
+
+
 # Generate a self-signed certificate for the CA
 openssl req -new -x509 -key $CA_KEY -out $CA_CRT -config $CA_CONF
 
 # Then sign the requesting certificate
 openssl x509 -req -in ${REQUEST_CSR} -CA ${CA_CRT} -CAkey ${CA_KEY} -CAcreateserial -out $REQUEST_CRT
 
+
+##
+## Import the signed certificate and the CA public cert 
+## into a java keyring.
+##
+
+## ... tbd
