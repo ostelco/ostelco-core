06051efaf199cc8b5106b42b6400e17448b427e9
Bjørn Remseth <la3lma@gmail.com>
2018-12-11 13:49:02 +0100

    Script to build everytning

---
+++ sim-administration/build-all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+DEPENDENCIES="mvn docker"
+
+for dep in $DEPENDENCIES ; do 
+ if [[ -z "$(which $dep)" ]] ; then
+   echo "Couldn't find dependency $dep"
+   exit 0
+ fi
+done
+
+
+#
+# Build all the maven artefacts
+#
+
+mvn clean install
+
+#
+# If that didn't go too well, then bail out.
+#
+
+if [[ $? -ne 0 ]] ; then echo 
+   echo "Compilation failed, aborting"
+   exit 1
+fi
+
+#
+# Build the dockerfile for the simadmin
+#
+docker build --file dockerfiles/Dockerfile.simadmin .
