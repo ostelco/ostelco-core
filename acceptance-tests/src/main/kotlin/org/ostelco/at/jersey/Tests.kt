b3cfdf95f3d50997dd99cab0bccdc01aca4cdf03
Prasanth Ullattil <prasanth@telenordigital.com>
2018-12-14 14:44:50 +0100

    Add more tests

--- acceptance-tests/src/main/kotlin/org/ostelco/at/jersey/Tests.kt
+++ acceptance-tests/src/main/kotlin/org/ostelco/at/jersey/Tests.kt
@@ -922,6 +922,89 @@ class eKYCTest {
         }
     }
 
+    @Test
+    fun `jersey test - ekyc verify 2 scans`() {
+
+        val email = "ekyc-${randomInt()}@test.com"
+        try {
+            createProfile(name = "Test User for eKYC", email = email)
+
+            val scanInfo: ScanInformation = get {
+                path = "/customer/new-ekyc-scanId"
+                subscriberId = email
+            }
+
+            assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
+
+            var dataMap = MultivaluedHashMap<String, String>()
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("idScanStatus", listOf("ERROR"))
+            dataMap.put("verificationStatus", listOf("FRAUD"))
+            dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
+            dataMap.put("idType", listOf("LICENSE"))
+            dataMap.put("idCountry", listOf("NOR"))
+            dataMap.put("idFirstName", listOf("Test User"))
+            dataMap.put("idLastName", listOf("Test Family"))
+            dataMap.put("idDob", listOf("1990-12-09"))
+            dataMap.put("merchantIdScanReference", listOf(scanInfo.scanId))
+
+            post<ScanInformation>(expectedResultCode = 200, dataType = MediaType.APPLICATION_FORM_URLENCODED_TYPE) {
+                path = "/ekyc/callback"
+                body = dataMap
+            }
+
+            val subscriberState: SubscriberState = get {
+                path = "/customer/subscriberState"
+                subscriberId = email
+            }
+            assertEquals("EKYC_REJECTED", subscriberState.status, message = "Wrong state")
+
+            val newScanInfo: ScanInformation = get {
+                path = "/customer/new-ekyc-scanId"
+                subscriberId = email
+            }
+
+            assertNotNull(newScanInfo.scanId, message = "Failed to get new scanId")
+
+            dataMap.clear()
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("idScanStatus", listOf("SUCCESS"))
+            dataMap.put("verificationStatus", listOf("VERIFIED"))
+            dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
+            dataMap.put("idType", listOf("LICENSE"))
+            dataMap.put("idCountry", listOf("NOR"))
+            dataMap.put("idFirstName", listOf("Test User"))
+            dataMap.put("idLastName", listOf("Test Family"))
+            dataMap.put("idDob", listOf("1990-12-09"))
+            dataMap.put("merchantIdScanReference", listOf(newScanInfo.scanId))
+
+            post<ScanInformation>(expectedResultCode = 200, dataType = MediaType.APPLICATION_FORM_URLENCODED_TYPE) {
+                path = "/ekyc/callback"
+                body = dataMap
+            }
+
+            val newSsubscriberState: SubscriberState = get {
+                path = "/customer/subscriberState"
+                subscriberId = email
+            }
+            assertEquals("EKYC_APPROVED", newSsubscriberState.status, message = "Wrong state")
+
+            val encodedEmail = URLEncoder.encode(email, "UTF-8")
+            val scanInformationList = get<Collection<ScanInformation>> {
+                path = "/profiles/$encodedEmail/scans"
+                subscriberId = email
+            }
+            assertEquals(2, scanInformationList.size, message = "More scans than expected")
+            var verifiedItemIndex = 0
+            if (newScanInfo.scanId == scanInformationList.elementAt(1).scanId) {
+                verifiedItemIndex = 1
+            }
+            assertEquals("APPROVED", scanInformationList.elementAt(verifiedItemIndex).status, message = "Wrong status")
+        } finally {
+            StripePayment.deleteCustomer(email = email)
+        }
+    }
+
 }
 
 class AnalyticsTest {
