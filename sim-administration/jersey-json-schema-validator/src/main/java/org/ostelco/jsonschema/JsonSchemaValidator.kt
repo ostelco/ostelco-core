1860a7b5937719d63f793e5567050490cca6d057
Bjørn Remseth <la3lma@gmail.com>
2018-12-18 14:30:05 +0100

    Whitespace

--- sim-administration/jersey-json-schema-validator/src/main/java/org/ostelco/jsonschema/JsonSchemaValidator.kt
+++ sim-administration/jersey-json-schema-validator/src/main/java/org/ostelco/jsonschema/JsonSchemaValidator.kt
@@ -50,7 +50,7 @@ class JsonSchemaValidator {
     }
 
     @Throws(WebApplicationException::class)
-    fun validateString(payloadClass: Class<*>, body: String, error: Response.Status) {
+    fun validateSring(payloadClass: Class<*>, body: String, error: Response.Status) {
         val schemaAnnotation = payloadClass.getAnnotation<JsonSchema>(JsonSchema::class.java)
         if (schemaAnnotation != null) {
             try {
