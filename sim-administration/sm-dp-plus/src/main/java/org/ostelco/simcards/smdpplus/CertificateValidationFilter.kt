9279e5fac5b79022395f04b58a0a771fa40d6430
Bjørn Remseth <la3lma@gmail.com>
2019-01-07 08:50:25 +0100

    Comment out the parts that don't work while they are being worked on

--- sim-administration/sm-dp-plus/src/main/java/org/ostelco/simcards/smdpplus/CertificateValidationFilter.kt
+++ sim-administration/sm-dp-plus/src/main/java/org/ostelco/simcards/smdpplus/CertificateValidationFilter.kt
@@ -12,6 +12,7 @@ import javax.ws.rs.container.PreMatching
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
 
+
 /**
  * A ContainerRequestFilter to do certificate validation beyond the tls validation.
  * For example, the filter matches the subject against a regex and will 403 if it doesn't match
@@ -43,28 +44,71 @@ class CertificateValidationFilter
     // Although this is a class level field, Jersey actually injects a proxy
     // which is able to simultaneously serve more requests.
     @Context
-    private val request: HttpServletRequest? = null
+    private var request: HttpServletRequest? = null
 
     init {
         this.dnRegex = Pattern.compile(dnRegex)
     }
 
-    @Throws(IOException::class)
-    override fun filter(requestContext: ContainerRequestContext) {
+    private fun certifcateMatches(requestContext: ContainerRequestContext): Boolean {
         val certificateChain = request!!.getAttribute(X509_CERTIFICATE_ATTRIBUTE) as Array<X509Certificate>
 
         if (certificateChain == null || certificateChain.size == 0 || certificateChain[0] == null) {
             requestContext.abortWith(buildForbiddenResponse("No certificate chain found!"))
-            return
+            return false
         }
 
         // The certificate of the client is always the first in the chain.
         val clientCert = certificateChain[0]
         val clientCertDN = clientCert.subjectDN.name
 
-        if (!dnRegex.matcher(clientCertDN).matches()) {
-            requestContext.abortWith(buildForbiddenResponse("Certificate subject is not recognized!"))
-        }
+        // XXX Don't use regexp matching.  Parse the clientCertDN into constituents, and then require
+        //     perfect match, and based on the matching patterh infer
+        //     which party is using the certificate to authenticate.  It might also be a decent idea
+        //     to require a fingerprint match of the cert.
+        return dnRegex.matcher(clientCertDN).matches()
+    }
+
+
+    @Throws(IOException::class)
+    override fun filter(requestContext: ContainerRequestContext) {
+
+
+/* Commented out while being debugged
+        val method = requestContext.request.
+        //Access allowed for all
+        if (!method.isAnnotationPresent(PermitAll::class.java)) {
+            //Access denied for all
+            if (method.isAnnotationPresent(DenyAll::class.java)) {
+                requestContext.abortWith(buildForbiddenResponse("Method not permitted"))
+                return
+            }
+
+            // XXX Not the final word in matching, should do more once we figure out  a way toi
+            //     infer user from certificate.
+            if (!certifcateMatches(requestContext)) {
+                requestContext.abortWith(buildForbiddenResponse("Certificate subject is not recognized!"))
+                return
+            }
+
+            //Split username and password tokens
+            val tokenizer = StringTokenizer(usernameAndPassword, ":")
+            val username = "some username we will eventually get from the cert"
+
+            //Verify user access
+            if (method.isAnnotationPresent(RolesAllowed::class.java)) {
+                method.
+                val rolesAnnotation = method.getAnnotation(RolesAllowed::class.java)
+                rolesAnnotation.
+                val rolesSet = HashSet<String>(Arrays.asList(rolesAnnotation.value()))
+
+                //Is user valid?
+                if (!isUserAllowed(username, password, rolesSet)) {
+                    requestContext.abortWith(ACCESS_DENIED)
+                    return
+                }
+            }
+        } */
     }
 
     private fun buildForbiddenResponse(message: String): Response {
@@ -74,7 +118,6 @@ class CertificateValidationFilter
     }
 
     companion object {
-
         private val X509_CERTIFICATE_ATTRIBUTE = "javax.servlet.request.X509Certificate"
     }
 }
