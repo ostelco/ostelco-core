f109d19ac36008e5e8ddb229e777cd0dd2844f8f
Bjørn Remseth <la3lma@gmail.com>
2019-01-11 00:31:13 +0100

    Rewrite ES2+ client to use apache HTTP client.  That fails, but that's where we are now.

---
+++ sim-administration/sm-dp-plus/src/test/java/org/ostelco/simcards/smdpplus/EncryptedEs2PlusTest.kt
@@ -0,0 +1,54 @@
+package org.ostelco.simcards.smdpplus
+
+import io.dropwizard.testing.DropwizardTestSupport
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.ostelco.sim.es2plus.ES2PlusClient
+
+class EncryptedEs2PlusTest {
+
+    @Before
+    fun setUp() {
+        SUPPORT.before()
+    }
+
+    @After
+    fun tearDown() {
+        SUPPORT.after()
+    }
+
+    /**
+     * These are the two scenarios that needs to be run in sequence from a
+     * sim-adminstrator, in pretty much the same way as it being run here.
+     */
+    @Test
+    fun handleHappyDayScenario() {
+        val client: ES2PlusClient =
+                SUPPORT.getApplication<SmDpPlusApplication>().es2plusClient
+        val eid = "123456789801234567890123456789012"
+        val iccid = "8901000000000000001"
+        val downloadResponse = client.downloadOrder(eid = eid, iccid = iccid, profileType = "foo")
+
+        // XXX Check for correct return values
+
+        val matchingCode = "ABCD-EFGH-01234-5679-0987-6543"
+        val confirmResponse =
+                client.confirmOrder(
+                        eid = eid,  // XXX Should be null -> Not part of the invocation
+                        iccid = iccid,
+                        confirmationCode = "4711", // should be null
+                        matchingId = matchingCode,  // XXX Should be null -> Not part of the invocation
+                        smdsAddress = "localhost", // XXX Should be null -> Not part of the invocation
+                        releaseFlag = true)
+
+        // XXX Check return values
+    }
+
+    companion object {
+        val SUPPORT = DropwizardTestSupport<SmDpPlusAppConfiguration>(
+                SmDpPlusApplication::class.java,
+                "config.yml"
+        )
+    }
+}
