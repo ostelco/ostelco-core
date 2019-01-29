d065942e137d9dce5881c892015d031529604da9
sami-alajrami <sami@praqma.com>
2019-01-29 10:59:45 +0100

    changing base64 flag to work on mac and linux

--- scripts/distribute-pantel-secrets.sh
+++ scripts/distribute-pantel-secrets.sh
@@ -15,7 +15,7 @@ echo; echo "======> Creating pantel-prod.json file, using the env variable PANTE
 for LOCATION in $(find . -name .gitignore  -exec grep pantel-prod.json  '{}' '+' ); do
   DIR_NAME=$(dirname $LOCATION)
   echo "Creating secrets file: ${DIR_NAME}/pantel-prod.json ..."
-  echo ${PANTEL_SECRETS_FILE} | base64 -d >  ${DIR_NAME}/pantel-prod.json
+  echo ${PANTEL_SECRETS_FILE} | base64 --decode >  ${DIR_NAME}/pantel-prod.json
   ls -l ${DIR_NAME}/pantel-prod.json
 done
 echo ''
