39ec1999d8b39b53b0eb85aafa62cafae732c7ee
Prasanth Ullattil <prasanth@telenordigital.com>
2019-02-07 10:50:48 +0100

    Use credentials for KMS in tink key init

--- scaninfo-datastore/src/main/kotlin/org/ostelco/prime/storage/scaninfo/ScanInfoEncrypt.kt
+++ scaninfo-datastore/src/main/kotlin/org/ostelco/prime/storage/scaninfo/ScanInfoEncrypt.kt
@@ -5,16 +5,25 @@ import com.google.crypto.tink.JsonKeysetReader
 import com.google.crypto.tink.KeysetHandle
 import com.google.crypto.tink.hybrid.HybridEncryptFactory
 import com.google.crypto.tink.integration.gcpkms.GcpKmsClient
+import org.ostelco.prime.getLogger
 import java.io.File
 
 class ScanInfoEncrypt(val keysetFilename: String, val masterKeyUri: String?) {
+    private val logger by getLogger()
     private val keysetHandle: KeysetHandle
     init {
+        logger.info(" GOOGLE_APPLICATION_CREDENTIALS = ${System.getenv("GOOGLE_APPLICATION_CREDENTIALS")}")
         if (masterKeyUri != null) {
+            val decryptor = GcpKmsClient().getAead(masterKeyUri)
+            if (decryptor == null) {
+                logger.info("Decryptor for ${masterKeyUri} failed")
+            } else {
+                logger.info("Decryptor for ${masterKeyUri} created successfully")
+            }
             // Decrypt the keyset using GCP master key
             keysetHandle = KeysetHandle.read(
                     JsonKeysetReader.withFile(File(keysetFilename)),
-                    GcpKmsClient().getAead(masterKeyUri))
+                    GcpKmsClient().withDefaultCredentials().getAead(masterKeyUri))
         } else {
             // Use local configuration directly
             keysetHandle = CleartextKeysetHandle.read(
