7163a1b62485715dbf29c280cb4c57403a5d5142
sami-alajrami <sami@praqma.com>
2019-02-12 13:03:28 +0100

    updating base64 flag to work on mac and linux

--- scripts/distribute-google-secrets.sh
+++ scripts/distribute-google-secrets.sh
@@ -16,7 +16,7 @@ echo; echo "======> Creating pantel-prod.json file, using the env variable PRIME
 for LOCATION in $(find . -name .gitignore  -exec grep pantel-prod.json  '{}' '+' ); do
   DIR_NAME=$(dirname $LOCATION)
   echo "Creating secrets file: ${DIR_NAME}/pantel-prod.json ..."
-  echo ${PRIME_GOOGLE_SECRETS_FILE} | base64 -d >  ${DIR_NAME}/pantel-prod.json
+  echo ${PRIME_GOOGLE_SECRETS_FILE} | base64 --decode >  ${DIR_NAME}/pantel-prod.json
   ls -l ${DIR_NAME}/pantel-prod.json
 done
 echo ''
