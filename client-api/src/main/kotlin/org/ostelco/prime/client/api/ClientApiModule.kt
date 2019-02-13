ostelco/ostelco-core

commit d84adcc5dde599f740bede4ca44b5eaaaf6ab0e2

Semi-automatic code fixups from Intellij

client-api/src/main/kotlin/org/ostelco/prime/client/api/ClientApiModule.kt
@@ -14,7 +14,6 @@ import org.ostelco.prime.client.api.resources.ProductsResource
 import org.ostelco.prime.client.api.resources.ProfileResource
 import org.ostelco.prime.client.api.resources.PurchaseResource
 import org.ostelco.prime.client.api.resources.ReferralResource
-import org.ostelco.prime.client.api.resources.SubscriptionResource
 import org.ostelco.prime.client.api.resources.SubscriptionsResource
 import org.ostelco.prime.client.api.store.SubscriberDAOImpl
 import org.ostelco.prime.module.PrimeModule
@@ -52,7 +51,7 @@ class ClientApiModule : PrimeModule {
         jerseyEnv.register(ProfileResource(dao))
         jerseyEnv.register(ReferralResource(dao))
         jerseyEnv.register(PaymentSourcesResource(dao))
-        jerseyEnv.register(SubscriptionResource(dao))
+        jerseyEnv.register(CustomerResource(dao))
         jerseyEnv.register(BundlesResource(dao))
         jerseyEnv.register(SubscriptionsResource(dao))
         jerseyEnv.register(CustomerResource(dao))

acceptance-tests/src/main/kotlin/org/ostelco/at/jersey/Tests.kt
@@ -694,7 +694,7 @@ class eKYCTest {
             assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
 
             var dataMap = MultivaluedHashMap<String,String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("ERROR"))
             dataMap.put("verificationStatus", listOf("DENIED_FRAUD"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -735,7 +735,7 @@ class eKYCTest {
             assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
 
             var dataMap = MultivaluedHashMap<String,String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("SUCCESS"))
             dataMap.put("verificationStatus", listOf("APPROVED_VERIFIED"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -779,7 +779,7 @@ class eKYCTest {
             assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
 
             var dataMap = MultivaluedHashMap<String,String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("SUCCESS"))
             dataMap.put("verificationStatus", listOf("APPROVED_VERIFIED"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -823,7 +823,7 @@ class eKYCTest {
             assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
 
             var dataMap = MultivaluedHashMap<String,String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("SUCCESS"))
             dataMap.put("verificationStatus", listOf("APPROVED_VERIFIED"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -865,7 +865,7 @@ class eKYCTest {
             assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
 
             var dataMap = MultivaluedHashMap<String,String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("SUCCESS"))
             dataMap.put("verificationStatus", listOf("APPROVED_VERIFIED"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -907,7 +907,7 @@ class eKYCTest {
             assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
 
             var dataMap = MultivaluedHashMap<String, String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("ERROR"))
             dataMap.put("verificationStatus", listOf("DENIED_FRAUD"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -938,7 +938,7 @@ class eKYCTest {
 
 
             dataMap.clear()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("SUCCESS"))
             dataMap.put("verificationStatus", listOf("APPROVED_VERIFIED"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -985,7 +985,7 @@ class eKYCTest {
             assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
 
             var dataMap = MultivaluedHashMap<String, String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("SUCCESS"))
             dataMap.put("verificationStatus", listOf("APPROVED_VERIFIED"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -1039,7 +1039,7 @@ class eKYCTest {
             assertNotNull(scanInfo.scanId, message = "Failed to get new scanId")
 
             var dataMap = MultivaluedHashMap<String, String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("ERROR"))
             dataMap.put("verificationStatus", listOf("DENIED_FRAUD"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -1069,7 +1069,7 @@ class eKYCTest {
             assertNotNull(newScanInfo.scanId, message = "Failed to get new scanId")
 
             dataMap.clear()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("SUCCESS"))
             dataMap.put("verificationStatus", listOf("APPROVED_VERIFIED"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))
@@ -1127,7 +1127,7 @@ class eKYCTest {
             val rejectReason="""{ "rejectReasonCode":"100", "rejectReasonDescription":"MANIPULATED_DOCUMENT", "rejectReasonDetails": [{ "detailsCode": "1001", "detailsDescription": "PHOTO" },{ "detailsCode": "1004", "detailsDescription": "DOB" }]}"""
 
             var dataMap = MultivaluedHashMap<String, String>()
-            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()));
+            dataMap.put("jumioIdScanReference", listOf(UUID.randomUUID().toString()))
             dataMap.put("idScanStatus", listOf("ERROR"))
             dataMap.put("verificationStatus", listOf("DENIED_FRAUD"))
             dataMap.put("callbackDate", listOf("2018-12-07T09:19:07.036Z"))

admin-api/src/main/kotlin/org/ostelco/prime/admin/api/HoustonResources.kt
@@ -150,7 +150,7 @@ class ProfilesResource {
     private fun isEmail(email: String): Boolean {
         val regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"
         val pattern = Pattern.compile(regex)
-        return pattern.matcher(email).matches();
+        return pattern.matcher(email).matches()
     }
 
     private fun getProfileForMsisdn(msisdn: String): Either<ApiError, Subscriber> {
@@ -443,7 +443,7 @@ class NotifyResource {
  * Resource used to handle plans related REST calls.
  */
 @Path("/plans")
-class PlanResource() {
+class PlanResource {
 
     private val logger by getLogger()
     private val storage by lazy { getResource<AdminDataSource>() }

admin-api/src/main/kotlin/org/ostelco/prime/admin/api/KYCResource.kt
@@ -98,7 +98,7 @@ class KYCResource {
         } catch (e: IOException) {
             logger.error("Cannot parse Json Data: $jsonData")
         }
-        return null;
+        return null
     }
 
     private fun toScanInformation(dataMap: Map<String, String>): ScanInformation? {
@@ -149,7 +149,7 @@ class KYCResource {
         }
         catch (e: NullPointerException) {
             logger.error("Missing mandatory fields in scan result ${dataMap}")
-            return null;
+            return null
         }
     }
 

analytics-module/src/main/kotlin/org/ostelco/prime/analytics/publishers/DelegatePubSubPublisher.kt
@@ -33,7 +33,7 @@ class DelegatePubSubPublisher(
             Publisher.newBuilder(topicName)
                     .setChannelProvider(channelProvider)
                     .setCredentialsProvider(NoCredentialsProvider())
-                    .build();
+                    .build()
         } else {
             Publisher.newBuilder(topicName).build()
         }

auth-server/src/integration-test/kotlin/org/ostelco/auth/AuthServerTest.kt
@@ -26,7 +26,7 @@ class AuthServerTest {
     fun testAuthServer() {
 
         val response = JerseyClientBuilder().build()
-                ?.target("http://0.0.0.0:${RULE.getLocalPort()}/auth/token")
+                ?.target("http://0.0.0.0:${RULE.localPort}/auth/token")
                 ?.request()
                 ?.header("X-MSISDN", msisdn)
                 ?.get()

bq-metrics-extractor/src/main/kotlin/org/ostelco/bqmetrics/BqMetricsExtractorApplication.kt
@@ -177,7 +177,7 @@ abstract class MetricBuilder(
      * Function to expand the environment variables in the SQL.
      */
     fun expandSql(): String {
-        val regex:Regex = "\\$\\{\\S*?\\}".toRegex(RegexOption.MULTILINE);
+        val regex:Regex = "\\$\\{\\S*?\\}".toRegex(RegexOption.MULTILINE)
         val expandedSql = regex.replace(sql) {it: MatchResult ->
             // The variable is of the format ${VAR}
             // extract variable name
@@ -203,13 +203,13 @@ abstract class MetricBuilder(
                 QueryJobConfiguration.newBuilder(
                         expandSql())
                         .setUseLegacySql(false)
-                        .build();
+                        .build()
 
         // Create a job ID so that we can safely retry.
-        val jobId: JobId = JobId.of(UUID.randomUUID().toString());
-        var queryJob: BQJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+        val jobId: JobId = JobId.of(UUID.randomUUID().toString())
+        var queryJob: BQJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build())
 
-       // Wait for the query to complete.
+        // Wait for the query to complete.
         // Retry maximum 4 times for up to 2 minutes.
         queryJob = async {
             queryJob.waitFor(
@@ -217,16 +217,16 @@ abstract class MetricBuilder(
                     RetryOption.retryDelayMultiplier(2.0),
                     RetryOption.maxRetryDelay(Duration.ofSeconds(20)),
                     RetryOption.maxAttempts(5),
-                    RetryOption.totalTimeout(Duration.ofMinutes(2)));
+                    RetryOption.totalTimeout(Duration.ofMinutes(2)))
         }.await()
 
         // Check for errors
         if (queryJob == null) {
-            throw  BqMetricsExtractionException("Job no longer exists");
-        } else if (queryJob.getStatus().getError() != null) {
+            throw  BqMetricsExtractionException("Job no longer exists")
+        } else if (queryJob.status.error != null) {
             // You can also look at queryJob.getStatus().getExecutionErrors() for all
             // errors, not just the latest one.
-            throw BqMetricsExtractionException(queryJob.getStatus().getError().toString());
+            throw BqMetricsExtractionException(queryJob.status.error.toString())
         }
         val result = queryJob.getQueryResults()
         if (result.totalRows != 1L) {

client-api/src/test/kotlin/org/ostelco/prime/client/api/resources/SubscriptionResourceTest.kt
@@ -89,7 +89,7 @@ class SubscriptionResourceTest {
                                 .setPrefix("Bearer")
                                 .buildAuthFilter()))
                 .addResource(AuthValueFactoryProvider.Binder(AccessTokenPrincipal::class.java))
-                .addResource(SubscriptionResource(DAO))
+                .addResource(CustomerResource(DAO))
                 .build()
     }
 }

commit 111687a8b29acb45c24555851ec72b8825f10be0

Fix the bug introduced by d84adcc5dde599f740bede4ca44b5eaaaf6ab0e2

client-api/src/main/kotlin/org/ostelco/prime/client/api/ClientApiModule.kt
@@ -4,17 +4,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName
 import io.dropwizard.setup.Environment
 import org.eclipse.jetty.servlets.CrossOriginFilter
 import org.ostelco.prime.client.api.metrics.reportMetricsAtStartUp
-import org.ostelco.prime.client.api.resources.AnalyticsResource
-import org.ostelco.prime.client.api.resources.ApplicationTokenResource
-import org.ostelco.prime.client.api.resources.BundlesResource
-import org.ostelco.prime.client.api.resources.ConsentsResource
-import org.ostelco.prime.client.api.resources.CustomerResource
-import org.ostelco.prime.client.api.resources.PaymentSourcesResource
-import org.ostelco.prime.client.api.resources.ProductsResource
-import org.ostelco.prime.client.api.resources.ProfileResource
-import org.ostelco.prime.client.api.resources.PurchaseResource
-import org.ostelco.prime.client.api.resources.ReferralResource
-import org.ostelco.prime.client.api.resources.SubscriptionsResource
+import org.ostelco.prime.client.api.resources.*
 import org.ostelco.prime.client.api.store.SubscriberDAOImpl
 import org.ostelco.prime.module.PrimeModule
 import java.util.*
@@ -51,7 +41,7 @@ class ClientApiModule : PrimeModule {
         jerseyEnv.register(ProfileResource(dao))
         jerseyEnv.register(ReferralResource(dao))
         jerseyEnv.register(PaymentSourcesResource(dao))
-        jerseyEnv.register(CustomerResource(dao))
+        jerseyEnv.register(SubscriptionResource(dao))
         jerseyEnv.register(BundlesResource(dao))
         jerseyEnv.register(SubscriptionsResource(dao))
         jerseyEnv.register(CustomerResource(dao))