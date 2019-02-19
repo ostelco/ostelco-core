f2a091747056f24a7d2ebe4a9d21fde1a27c15e2
Bjørn Remseth <la3lma@gmail.com>
2019-02-19 11:41:46 +0100

    Outline how to get the thing done

--- sim-administration/simmanager/src/main/java/org/ostelco/simcards/admin/PeriodicProvisioningTask.kt
+++ sim-administration/simmanager/src/main/java/org/ostelco/simcards/admin/PeriodicProvisioningTask.kt
@@ -9,6 +9,14 @@ class PreallocateProfiles() : Task("preallocate_sim_profiles") {
 
     @Throws(Exception::class)
     override fun execute(parameters: ImmutableMultimap<String, String>, output: PrintWriter) {
-        // Do stuff here
+        // TODO:
+        // * List all of the HLRs
+        // * For each HLR, figure out
+        //    - How many profiles are available for respectively android and default
+        //    - If the numbers are below low-water marks, preallocate a chunk.  That chunk
+        //      should be sufficiently small to ensure that the task will terminate in a reasonable
+        //      amount of time
+        //    - If the amount of available SIMs are getting low (for some definition of low), then
+        //      sound the alert via logging or some other method.
     }
 }
