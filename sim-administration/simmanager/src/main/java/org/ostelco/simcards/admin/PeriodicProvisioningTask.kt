faa71cc174062e0f1ea7dd9774e0b8923f184231
Bjørn Remseth <la3lma@gmail.com>
2019-02-22 11:09:43 +0100

    Make it public again, since the internal thing failed during compilation

--- sim-administration/simmanager/src/main/java/org/ostelco/simcards/admin/PeriodicProvisioningTask.kt
+++ sim-administration/simmanager/src/main/java/org/ostelco/simcards/admin/PeriodicProvisioningTask.kt
@@ -62,11 +62,11 @@ class PreallocateProfilesTask(
 
 
     /**
-     * Made internal to be testable.   Perform
+     * Made public to be testable.   Perform
      * allocation of profiles so that if possible, there will be tasks available for
      * provisioning.
      */
-    internal fun preallocateProfiles() {
+    public fun preallocateProfiles() {
         var hlrs: Collection<HlrAdapter> = simInventoryDAO.getHlrAdapters()
 
         for (hlr in hlrs) {
