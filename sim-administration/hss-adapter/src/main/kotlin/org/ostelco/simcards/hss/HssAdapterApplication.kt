ca3dd2cfbff91b4ef33849fa857ca6e5bd87d7c9
Bjørn Remseth <la3lma@gmail.com>
2019-03-15 16:12:04 +0100

    First stab at gluing the grpc and the hss parts together.

--- sim-administration/hss-adapter/src/main/kotlin/org/ostelco/simcards/hss/HssAdapterApplication.kt
+++ sim-administration/hss-adapter/src/main/kotlin/org/ostelco/simcards/hss/HssAdapterApplication.kt
@@ -7,10 +7,21 @@ import io.dropwizard.Configuration
 import io.dropwizard.client.HttpClientBuilder
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
+import io.grpc.stub.StreamObserver
 import org.ostelco.simcards.admin.ConfigRegistry
 import org.ostelco.simcards.admin.HssConfig
+import org.ostelco.simcards.admin.mapRight
+import org.ostelco.simcards.hss.profilevendors.api.ActivationRequest
+import org.ostelco.simcards.hss.profilevendors.api.HssServiceGrpc.HssServiceImplBase
+import org.ostelco.simcards.hss.profilevendors.api.HssServiceResponse
+import org.ostelco.simcards.hss.profilevendors.api.SuspensionRequest
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
+import io.grpc.ServerBuilder
+import io.dropwizard.lifecycle.Managed
+import io.grpc.Server
+import org.apache.http.impl.client.CloseableHttpClient
+
 
 fun main(args: Array<String>) = HssAdapterApplication().run(*args)
 
@@ -62,9 +73,59 @@ class HssAdapterApplication : Application<HssAdapterApplicationConfiguration>() 
          *    getting.
          */
 
+
+        val myHssService = ManagedHssService(
+                port = 9000,
+                env = env,
+                httpClient = httpClient,
+                configuration = configuration.hssVendors)
+
+        env.lifecycle().manage(myHssService)
+
+        // This dispatcher  is what we will use to handle the incoming
+        // requests.  it will essentially do all the work.
+        // When it has been proven to work, we will make it something that can
+        // be built in a separate reposítory, preferably using a library mechanism.
+    }
+}
+
+
+class ManagedGrpcService (private val port: Int,
+                          private val service: io.grpc.BindableService ) : Managed {
+
+    private var server: Server
+
+    init {
+        this.server = ServerBuilder.forPort(port)
+                .addService(service)
+                .build()
+    }
+
+
+    @Throws(Exception::class)
+    override fun start() {
+        // Start the server
+        server.start()
+    }
+
+    @Throws(Exception::class)
+    override fun stop() {
+        server.awaitTermination()
+    }
+}
+
+class ManagedHssService(
+        private val configuration: List<HssConfig>,
+        private val env: Environment,
+        private val httpClient: CloseableHttpClient,
+        private val port: Int) : Managed {
+
+    val managedService: ManagedGrpcService
+
+    init {
         val adapters = mutableSetOf<HssAdapter>()
 
-        for (config in configuration.hssVendors) {
+        for (config in configuration) {
             // Only a simple profilevendors added here, ut this is the extension point where we will
             // add other, proprietary adapters eventually.
             adapters.add(SimpleHssAdapter(name = config.name, httpClient = httpClient, config = config))
@@ -77,19 +138,39 @@ class HssAdapterApplication : Application<HssAdapterApplicationConfiguration>() 
                     }
                 })
 
+        val hssService = HssServiceImpl(dispatcher)
 
+        this.managedService = ManagedGrpcService(port = port, service = hssService)
 
+    }
 
+    @Throws(Exception::class)
+    override fun start() {
+        managedService.start()
+    }
 
-
-
-        // This dispatcher  is what we will use to handle the incoming
-        // requests.  it will essentially do all the work.
-        // When it has been proven to work, we will make it something that can
-        // be built in a separate reposítory, preferably using a library mechanism.
+    @Throws(Exception::class)
+    override fun stop() {
+        managedService.stop()
     }
 }
 
+
+class HssServiceImpl(val hssDispatcher: HssDispatcher) : HssServiceImplBase() {
+
+    override fun activate(request: ActivationRequest?, responseObserver: StreamObserver<HssServiceResponse>?) {
+        hssDispatcher.activate(hssName = request.hss, iccid = request.iccid, msisdn = request.msisdn)
+                .mapRight { responseObserver.onNext(HssServiceResponse.newBuilder().setSuccess(true).build()) }
+                .mapLeft { responseObserver.onNext(HssServiceResponse.newBuilder().setSuccess(false).build()) }
+
+    }
+
+    override fun suspend(request: SuspensionRequest?, responseObserver: StreamObserver<HssServiceResponse>?) {
+        hssDispatcher.suspend(hssName = request.hss, iccid = request.iccid)
+                .mapRight { responseObserver.onNext(HssServiceResponse.newBuilder().setSuccess(true).build()) }
+                .mapLeft { responseObserver.onNext(HssServiceResponse.newBuilder().setSuccess(false).build()) }
+    }
+}
 
 
 class HssAdapterApplicationConfiguration : Configuration() {
