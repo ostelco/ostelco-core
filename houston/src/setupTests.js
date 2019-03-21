de64058fa9de32969c40f25289332f38c1d1b7ee
Bjørn Remseth <la3lma@gmail.com>
2019-03-21 15:11:58 +0100

    Reverting change (that shouldn't have been done)

--- houston/src/setupTests.js
+++ houston/src/setupTests.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-profilevendors-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });
