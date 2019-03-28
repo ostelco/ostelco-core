0a07793090ac73f06f3f13378c62b0707c39f938
mpeterss <mape@comoyo.com>
2019-03-28 15:38:59 +0100

    Updated OCS to handle multiple rating groups

--- ocs-ktc/src/main/kotlin/org/ostelco/prime/ocs/core/OnlineCharging.kt
+++ ocs-ktc/src/main/kotlin/org/ostelco/prime/ocs/core/OnlineCharging.kt
@@ -15,6 +15,7 @@ import org.ostelco.prime.module.getResource
 import org.ostelco.prime.ocs.analytics.AnalyticsReporter
 import org.ostelco.prime.ocs.consumption.OcsAsyncRequestConsumer
 import org.ostelco.prime.storage.ClientDataSource
+import org.ostelco.prime.storage.ConsumptionResult
 
 object OnlineCharging : OcsAsyncRequestConsumer {
 
@@ -38,83 +39,97 @@ object OnlineCharging : OcsAsyncRequestConsumer {
                         .setMsisdn(msisdn)
                         .setResultCode(ResultCode.DIAMETER_SUCCESS)
 
-                if (request.msccCount > 0) {
-                    val mscc = request.getMscc(0)
-                    val requested = mscc?.requested?.totalOctets ?: 0
-                    val used = mscc?.used?.totalOctets ?: 0
 
-                    val responseMscc = MultipleServiceCreditControl
-                            .newBuilder(mscc)
-                            .setValidityTime(86400)
+                request.msccList.forEach { mscc ->
 
-
-                    storage.consume(msisdn, used, requested) { storeResult ->
-                        storeResult.fold(
-                            {
-                                // TODO martin : Should we handle all errors as NotFoundError?
-                                response.resultCode = ResultCode.DIAMETER_USER_UNKNOWN
-                                synchronized(OnlineCharging) {
-                                    returnCreditControlAnswer(response.build())
-                                }
-                            },
-                            {
-                                consumptionResult ->
-
-                                val grantedTotalOctets = if (mscc.reportingReason != ReportingReason.FINAL
-                                        && mscc.requested.totalOctets > 0) {
-
-                                    if (consumptionResult.granted < mscc.requested.totalOctets) {
-                                        responseMscc.finalUnitIndication = FinalUnitIndication.newBuilder()
-                                                .setFinalUnitAction(FinalUnitAction.TERMINATE)
-                                                .setIsSet(true)
-                                                .build()
+                    val requested = mscc.requested?.totalOctets ?: 0
+                    val used = mscc.used?.totalOctets ?: 0
+                    if (shouldConsume(mscc.ratingGroup, mscc.serviceIdentifier)) {
+                        storage.consume(msisdn, used, requested) { storeResult ->
+                            storeResult.fold(
+                                    {
+                                        response.resultCode = ResultCode.DIAMETER_USER_UNKNOWN
+                                    },
+                                    {
+                                        consumptionResult -> addGrantedQuota(consumptionResult.granted, mscc, response)
+                                        reportAnalytics(consumptionResult, request)
                                     }
-
-                                    consumptionResult.granted
-
-                                } else {
-                                    // Use -1 to indicate no granted service unit should be included in the answer
-                                    -1
-                                }
-
-                                responseMscc.granted = ServiceUnit.newBuilder().setTotalOctets(grantedTotalOctets).build()
-
-                                if (grantedTotalOctets > 0) {
-                                    responseMscc.quotaHoldingTime = 7200
-                                    responseMscc.volumeQuotaThreshold = (grantedTotalOctets * 0.8).toLong() // 80%
-                                }
-
-                                responseMscc.resultCode = ResultCode.DIAMETER_SUCCESS
-
-                                if (!loadUnitTest && !loadAcceptanceTest) {
-                                    launch {
-                                        AnalyticsReporter.report(
-                                                msisdnAnalyticsId = consumptionResult.msisdnAnalyticsId,
-                                                request = request,
-                                                bundleBytes = consumptionResult.balance)
-                                    }
-
-                                    // FIXME vihang: get customerId for MSISDN
-                                    /*launch {
-                                        Notifications.lowBalanceAlert(
-                                                customerId = msisdn,
-                                                reserved = consumptionResult.granted,
-                                                balance = consumptionResult.balance)
-                                    }*/
-                                }
-                                response.addMscc(responseMscc)
-                                synchronized(OnlineCharging) {
-                                    returnCreditControlAnswer(response.build())
-                                }
-                            })
+                            )
+                        }
+                    } else {
+                        // zeroRate
+                        addGrantedQuota(requested, mscc, response)
                     }
                 }
-                else {
-                    synchronized(OnlineCharging) {
-                        returnCreditControlAnswer(response.build())
-                    }
+                synchronized(OnlineCharging) {
+                    returnCreditControlAnswer(response.build())
                 }
             }
         }
+    }
+
+    private fun reportAnalytics(consumptionResult : ConsumptionResult, request: CreditControlRequestInfo) {
+        if (!loadUnitTest && !loadAcceptanceTest) {
+            CoroutineScope(Dispatchers.Default).launch {
+                AnalyticsReporter.report(
+                        msisdnAnalyticsId = consumptionResult.msisdnAnalyticsId,
+                        request = request,
+                        bundleBytes = consumptionResult.balance)
+            }
+
+            // FIXME vihang: get customerId for MSISDN
+            /*launch {
+            Notifications.lowBalanceAlert(
+                customerId = msisdn,
+                reserved = consumptionResult.granted,
+                balance = consumptionResult.balance)
+        }*/
+        }
+    }
+
+    private fun addGrantedQuota(granted: Long, mscc: MultipleServiceCreditControl, response: CreditControlAnswerInfo.Builder) {
+
+        val responseMscc = MultipleServiceCreditControl
+                .newBuilder(mscc)
+                .setValidityTime(86400)
+
+        val grantedTotalOctets = if (mscc.reportingReason != ReportingReason.FINAL
+                && mscc.requested.totalOctets > 0) {
+
+            if (granted < mscc.requested.totalOctets) {
+                responseMscc.finalUnitIndication = FinalUnitIndication.newBuilder()
+                        .setFinalUnitAction(FinalUnitAction.TERMINATE)
+                        .setIsSet(true)
+                        .build()
+            }
+            granted
+
+        } else {
+            // Use -1 to indicate no granted service unit should be included in the answer
+            -1
+        }
+
+        responseMscc.granted = ServiceUnit.newBuilder().setTotalOctets(grantedTotalOctets).build()
+
+        if (grantedTotalOctets > 0) {
+            responseMscc.quotaHoldingTime = 7200
+            responseMscc.volumeQuotaThreshold = (grantedTotalOctets * 0.8).toLong() // 80%
+        }
+
+        responseMscc.resultCode = ResultCode.DIAMETER_SUCCESS
+
+        response.addMscc(responseMscc.build())
+    }
+
+    private fun shouldConsume(ratingGroup: Long, serviceIdentifier: Long): Boolean {
+
+        // FixMe : Fetch list from somewhere ™
+        // For now hardcoded to known combinations
+
+        if (arrayOf(1L, 2L, 4L, 400L).contains(serviceIdentifier)) {
+            return true
+        }
+
+        return false
     }
 }
