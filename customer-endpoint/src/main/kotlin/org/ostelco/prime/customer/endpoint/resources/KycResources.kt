4f49478c713aebbc38bc4f9f04cfe05e64f2d591
Vihang Patil <vihang.patil@gmail.com>
2019-04-02 07:30:50 +0800

    Fixes in Customer API

--- customer-endpoint/src/main/kotlin/org/ostelco/prime/customer/endpoint/resources/KycResources.kt
+++ customer-endpoint/src/main/kotlin/org/ostelco/prime/customer/endpoint/resources/KycResources.kt
@@ -12,6 +12,7 @@ import javax.ws.rs.PUT
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
@@ -92,8 +93,11 @@ class SingaporeKycResource(private val dao: SubscriberDAO): KycResource(regionCo
     fun saveProfile(
             @Auth token: AccessTokenPrincipal?,
             @NotNull
-            @PathParam("authorisationCode")
-            authorisationCode: String): Response {
+            @QueryParam("address")
+            address: String,
+            @NotNull
+            @QueryParam("phoneNumber")
+            phoneNumber: String): Response {
 
         if (token == null) {
             return Response.status(Response.Status.UNAUTHORIZED)
@@ -126,7 +130,7 @@ class JumioKycResource(private val regionCode: String, private val dao: Subscrib
     }
 
     @GET
-    @Path("/scans/{scanId}/status")
+    @Path("/scans/{scanId}")
     @Produces(MediaType.APPLICATION_JSON)
     fun getScanStatus(
             @Auth token: AccessTokenPrincipal?,
