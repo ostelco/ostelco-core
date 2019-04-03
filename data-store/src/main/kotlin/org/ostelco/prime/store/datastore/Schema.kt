917816c35755bbff47734528a08e7dca72de11c0
Kjell M. Myksvoll <kmm@comoyo.com>
2019-04-03 09:31:57 +0200

    Adds support for storing 'null' to generic datastore

--- data-store/src/main/kotlin/org/ostelco/prime/store/datastore/Schema.kt
+++ data-store/src/main/kotlin/org/ostelco/prime/store/datastore/Schema.kt
@@ -3,13 +3,7 @@ package org.ostelco.prime.store.datastore
 import com.fasterxml.jackson.core.type.TypeReference
 import com.google.cloud.NoCredentials
 import com.google.cloud.Timestamp
-import com.google.cloud.datastore.Blob
-import com.google.cloud.datastore.Datastore
-import com.google.cloud.datastore.DatastoreOptions
-import com.google.cloud.datastore.FullEntity
-import com.google.cloud.datastore.Key
-import com.google.cloud.datastore.KeyFactory
-import com.google.cloud.datastore.LatLng
+import com.google.cloud.datastore.*
 import com.google.cloud.datastore.testing.LocalDatastoreHelper
 import com.google.cloud.http.HttpTransportOptions
 import org.ostelco.prime.jsonmapper.objectMapper
@@ -77,22 +71,26 @@ class EntityStore<T>(
 
     fun add(t: T): Key {
         // convert object to map of (field name, field value)
-        val map: Map<String, Any> = objectMapper.convertValue(t, object : TypeReference<Map<String, Any?>>() {})
+        val map: Map<String, Any?> = objectMapper.convertValue(t, object : TypeReference<Map<String, Any?>>() {})
 
         // Entity Builder
         val entity = FullEntity.newBuilder(keyFactory.newKey())
 
         // for each field, call appropriate setter
         map.forEach { key, value ->
-            when (value) {
-                is Key -> entity.set(key, value)
-                is Long -> entity.set(key, value)
-                is Blob -> entity.set(key, value)
-                is Double -> entity.set(key, value)
-                is Boolean -> entity.set(key, value)
-                is LatLng -> entity.set(key, value)
-                is String -> entity.set(key, value)
-                is Timestamp -> entity.set(key, value)
+            if (value == null) {
+                entity.set(key, NullValue.of())
+            } else {
+                when (value) {
+                    is Key -> entity.set(key, value)
+                    is Long -> entity.set(key, value)
+                    is Blob -> entity.set(key, value)
+                    is Double -> entity.set(key, value)
+                    is Boolean -> entity.set(key, value)
+                    is LatLng -> entity.set(key, value)
+                    is String -> entity.set(key, value)
+                    is Timestamp -> entity.set(key, value)
+                }
             }
         }
         return datastore.add(entity.build()).key
