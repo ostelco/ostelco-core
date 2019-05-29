d590da855d0544fccfa6b1969ae7101dcb18f3d2
Vihang Patil <vihang.patil@gmail.com>
2019-05-29 17:02:28 +0200

    Dependency updates

--- prime/script/start.sh
+++ prime/script/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# XXX REMOVE BEFORE FLIGHT (only for debugging
-#     idemia backchannel)
-
-nohup tcpdump -s 1500  portrange 8000-9000 -w dumpfile.pcap > dump.out &
-
-
 # Start app
 exec java \
     -Dfile.encoding=UTF-8 \
