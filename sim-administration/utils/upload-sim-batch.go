80109f7af2c0052721984223ae242c022f3e98e1
Bjørn Remseth <la3lma@gmail.com>
2019-06-13 09:08:24 +0200

    Fix bug: Fencepost (generated one profile to little)

--- sim-administration/utils/upload-sim-batch.go
+++ sim-administration/utils/upload-sim-batch.go
@@ -55,7 +55,7 @@ func generateCsvPayload(batch Batch) string {
 
 	var imsi = batch.firstImsi
 	var msisdn = batch.firstMsisdn
-	for i := 0; i < batch.length; i++ {
+	for i := 0; i <= batch.length; i++ {
 
 		iccid := fmt.Sprintf("%d%1d", iccidWithoutLuhnChecksum, luhnChecksum(iccidWithoutLuhnChecksum))
 		line := fmt.Sprintf("%s, %d, %d,,,,,%s\n", iccid, imsi, msisdn, batch.profileType)
