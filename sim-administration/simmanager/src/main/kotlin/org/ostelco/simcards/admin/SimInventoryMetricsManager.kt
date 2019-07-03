f2909b438b833030c4f90a38391aab98de53ffb7
Bjørn Remseth <la3lma@gmail.com>
2019-07-03 11:06:42 +0200

    Actually generating a (dummy) metric with a constant value of 42

--- sim-administration/simmanager/src/main/kotlin/org/ostelco/simcards/admin/SimInventoryMetricsManager.kt
+++ sim-administration/simmanager/src/main/kotlin/org/ostelco/simcards/admin/SimInventoryMetricsManager.kt
@@ -1,5 +1,6 @@
 package org.ostelco.simcards.admin
 
+import com.codahale.metrics.Gauge
 import com.codahale.metrics.MetricRegistry
 import io.dropwizard.lifecycle.Managed
 import org.ostelco.prime.getLogger
@@ -9,7 +10,12 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
 
 
-class SimInventoryMetricsManager(dao: SimInventoryDAO, metrics: MetricRegistry) : Managed {
+class SimInventoryMetricsManager(private val dao: SimInventoryDAO, private val metrics: MetricRegistry) : Managed {
+
+
+    ///
+    ///  Set up the metrics manager and prepare to run periodic task every five minutes.
+    ///
 
     private val logger by getLogger()
     val executorService = Executors.newScheduledThreadPool(5)
@@ -19,21 +25,52 @@ class SimInventoryMetricsManager(dao: SimInventoryDAO, metrics: MetricRegistry) 
     // Run every five minutes
     override fun start() {
         if (!isRunning.getAndSet(true)) {
-            executorService.scheduleAtFixedRate({
-                if (isRunning.get()) {
-                    try {
-                        logger.info("Periodic sim inventory metrics poller executing")
-                    } catch (Throwable t) {
-                        logger.warn("Periodic inventory metrics poller failed", t)
-                    }
-                }
-            }, 0L, 300L, TimeUnit.SECONDS)
+            startExecutorService()
         }
     }
 
+    // Stop when dropwizard is killed
     override fun stop() {
         if (isRunning.getAndSet(false)) {
             executorService.shutdownNow()
+        }
+    }
+
+    // Start an execution service that will run the periodicTask method every
+    // five minutes.
+    private fun startExecutorService() {
+        executorService.scheduleAtFixedRate({
+            if (isRunning.get()) {
+                try {
+                    periodicTask()
+                } catch (t: Throwable) {
+                    logger.warn("Periodic sim inventory metrics poller failed", t)
+                }
+            }
+        }, 0L, 300L, TimeUnit.SECONDS)
+    }
+
+
+    ///
+    /// Handle the periodic task.
+    ///
+
+
+    val runningForFirstTime = AtomicBoolean(true)
+
+    private fun periodicTask() {
+
+        logger.info("Periodic sim inventory metrics poller executing")
+
+        if (runningForFirstTime.getAndSet(false)) {
+            // Create a dummy metric, will later be done by the periodic task
+            // when needed.
+            metrics.register(MetricRegistry.name(SimInventoryMetricsManager::class.java, "dummyMetric", "noOfThings"),
+                    object : Gauge<Int> {
+                        override fun getValue(): Int {
+                            return 42
+                        }
+                    })
         }
     }
 }
