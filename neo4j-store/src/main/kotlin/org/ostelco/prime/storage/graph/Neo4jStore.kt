d524bc997f102a10ac13e8343b63ccd3f5ae359f
Prasanth Ullattil <prasanth@telenordigital.com>
2019-08-16 14:36:41 +0200

    Change log level for jumio message

--- neo4j-store/src/main/kotlin/org/ostelco/prime/storage/graph/Neo4jStore.kt
+++ neo4j-store/src/main/kotlin/org/ostelco/prime/storage/graph/Neo4jStore.kt
@@ -1605,7 +1605,7 @@ object Neo4jStoreSingleton : GraphStore {
                             body = FCMStrings.JUMIO_IDENTITY_FAILED.s,
                             data = extendedStatus
                     )
-                    logger.warn(NOTIFY_OPS_MARKER, "Jumio verification failed for ${customer.contactEmail} Info: $extendedStatus")
+                    logger.info(NOTIFY_OPS_MARKER, "Jumio verification failed for ${customer.contactEmail} Info: $extendedStatus")
                     setKycStatus(
                             customerId = customer.id,
                             regionCode = scanInformation.countryCode.toLowerCase(),
