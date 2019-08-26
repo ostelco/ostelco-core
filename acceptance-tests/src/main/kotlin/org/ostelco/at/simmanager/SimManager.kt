6424646b1b5793cd0748d349e7824cb4f23594e5
Bjørn Remseth <la3lma@gmail.com>
2019-08-26 16:00:19 +0200

    Comments and fixing dead imports

--- acceptance-tests/src/main/kotlin/org/ostelco/at/simmanager/SimManager.kt
+++ acceptance-tests/src/main/kotlin/org/ostelco/at/simmanager/SimManager.kt
@@ -1,26 +1,8 @@
 package org.ostelco.simcards.admin
 
 import arrow.core.Either
-import com.codahale.metrics.health.HealthCheck
-import com.google.gson.JsonElement
-import com.google.gson.JsonObject
-import com.google.gson.JsonParser
-import io.dropwizard.client.HttpClientBuilder
-import io.dropwizard.client.JerseyClientBuilder
 import io.dropwizard.jdbi3.JdbiFactory
-import io.dropwizard.testing.ConfigOverride
-import io.dropwizard.testing.ResourceHelpers
-import io.dropwizard.testing.junit.DropwizardAppRule
-import org.assertj.core.api.Assertions.assertThat
-import org.glassfish.jersey.client.ClientProperties
 import org.jdbi.v3.core.Jdbi
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertTrue
-import org.junit.Before
-import org.junit.BeforeClass
-import org.junit.ClassRule
-import org.junit.Ignore
 import org.junit.Test
 import org.ostelco.prime.simmanager.SimManagerError
 import org.ostelco.simcards.hss.DirectHssDispatcher
@@ -35,13 +17,8 @@ import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.FixedHostPortGenericContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
-import java.io.FileInputStream
-import java.time.Duration
-import java.time.temporal.ChronoUnit
-import javax.ws.rs.client.Client
-import javax.ws.rs.client.Entity
-import javax.ws.rs.core.MediaType
 
+// Trello card being used to manage development of this file: https://trello.com/c/48els9Gt
 
 class SimManager {
 
