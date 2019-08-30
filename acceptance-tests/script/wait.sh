5873b5baa96140884656bbe34d783893e17bf427
Bjørn Remseth <la3lma@gmail.com>
2019-08-30 14:08:08 +0200

    Add wait, and also set up the endpoint to  be the correcdt one.

--- acceptance-tests/script/wait.sh
+++ acceptance-tests/script/wait.sh
@@ -7,15 +7,21 @@ echo "AT waiting ocsgw to launch on 3868..."
 while ! nc -z 172.16.238.3 3868; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
+echo "      ocs  answers incoming http requests"
 
-echo "ocsgw launched"
 
 echo "AT waiting Prime to launch on 8080..."
 
 while ! nc -z prime 8080; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
+echo "       prime+  answers incoming http requests""
+
+echo "Waiting for sm-dp+ to be up"
+while ! nc -z smdp-plus-emulator 18080; do
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+echo "       sm-dp+  answers incoming http requests""
 
 echo "Prime launched"
-
 java -cp '/acceptance-tests.jar' org.junit.runner.JUnitCore org.ostelco.at.TestSuite
