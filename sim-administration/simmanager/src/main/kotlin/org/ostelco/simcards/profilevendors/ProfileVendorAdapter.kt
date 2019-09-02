692c41e5ea9340ded3cd05d27ddfe515d55e28ed
Bjørn Remseth <la3lma@gmail.com>
2019-09-02 15:46:55 +0200

    Fixing typos and brainos, at least some of them.

--- sim-administration/simmanager/src/main/kotlin/org/ostelco/simcards/profilevendors/ProfileVendorAdapter.kt
+++ sim-administration/simmanager/src/main/kotlin/org/ostelco/simcards/profilevendors/ProfileVendorAdapter.kt
@@ -92,7 +92,7 @@ data class ProfileVendorAdapter(
         val payload = mapper.writeValueAsString(body)
 
         val request = RequestBuilder.post()
-                .setUri("${config.es2plusEndpoint}/gsma/rsp2/es2plus/downloadOrder")
+                .setUri("${config.es2plusEndpoint}/downloadOrder")
                 .setHeader("User-Agent", "gsma-rsp-lpad")
                 .setHeader("X-Admin-Protocol", "gsma/rsp/v2.0.0")
                 .setHeader("Content-Type", MediaType.APPLICATION_JSON)
@@ -178,7 +178,7 @@ data class ProfileVendorAdapter(
         val payload = mapper.writeValueAsString(body)
 
         val request = RequestBuilder.post()
-                .setUri("${config.es2plusEndpoint}/gsma/rsp2/es2plus/confirmOrder")
+                .setUri("${config.es2plusEndpoint}/confirmOrder")
                 .setHeader("User-Agent", "gsma-rsp-lpad")
                 .setHeader("X-Admin-Protocol", "gsma/rsp/v2.0.0")
                 .setHeader("Content-Type", MediaType.APPLICATION_JSON)
