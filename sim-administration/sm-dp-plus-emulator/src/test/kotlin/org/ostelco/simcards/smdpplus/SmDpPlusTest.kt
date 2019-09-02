addd1167d2c95d5df2a317489c281aef96493cf0
Bjørn Remseth <la3lma@gmail.com>
2019-09-02 11:37:25 +0200

    Refactor client setup to class setup not method setup!

--- sim-administration/sm-dp-plus-emulator/src/test/kotlin/org/ostelco/simcards/smdpplus/SmDpPlusTest.kt
+++ sim-administration/sm-dp-plus-emulator/src/test/kotlin/org/ostelco/simcards/smdpplus/SmDpPlusTest.kt
@@ -6,9 +6,12 @@ import io.dropwizard.testing.junit.DropwizardAppRule
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import junit.framework.TestCase.fail
+import org.apache.http.impl.client.CloseableHttpClient
 import org.junit.ClassRule
 import org.junit.Test
 import org.ostelco.sim.es2plus.ES2PlusClient
+import org.ostelco.sim.es2plus.FunctionExecutionStatusType
+import org.ostelco.simcards.smdpplus.EncryptedEs2PlusTest.Companion.SUPPORT
 
 public class SmDpPlusTest {
 
@@ -19,6 +22,17 @@ public class SmDpPlusTest {
                 ResourceHelpers.resourceFilePath("config.yml"))
     }
 
+    private val httpClient: CloseableHttpClient
+    private val localPort: Int
+    private val client: ES2PlusClient
+
+    init {
+        this.httpClient = HttpClientBuilder(SM_DP_PLUS_RULE.environment).build("Test client")
+        this.localPort = SM_DP_PLUS_RULE.localPort
+        this.client = ES2PlusClient(httpClient = httpClient, port = localPort, requesterId = "Dunderhonning", useHttps = false)
+    }
+
+
     @Test
     fun testThatCorrectNumberOfProfilesAreLoaded() {
         val app: SmDpPlusApplication = SM_DP_PLUS_RULE.getApplication<SmDpPlusApplication>()
@@ -28,10 +42,6 @@ public class SmDpPlusTest {
 
     @Test
     fun testGettingInfo() {
-        val localPort = SM_DP_PLUS_RULE.localPort
-        val httpClient = HttpClientBuilder(SM_DP_PLUS_RULE.environment).build("Test client")
-        val client = ES2PlusClient(httpClient = httpClient, port = localPort, requesterId = "Dunderhonning", useHttps = false)
-
         val iccid = "8901000000000000001"
         val profileStatus = client.profileStatus(listOf(iccid))
         val profileStatusList = profileStatus.profileStatusList
@@ -45,8 +55,9 @@ public class SmDpPlusTest {
     }
 
 
-    /*
-       val client: ES2PlusClient =
+    @Test
+    fun testFullRoundtrip() {
+        val client: ES2PlusClient =
                 SUPPORT.getApplication<SmDpPlusApplication>().es2plusClient
         val eid = "12345678980123456789012345678901"
         val iccid = "8901000000000000001"
@@ -67,5 +78,5 @@ public class SmDpPlusTest {
         assertEquals(FunctionExecutionStatusType.ExecutedSuccess, confirmResponse.header.functionExecutionStatus.status)
         assertEquals(eid, confirmResponse.eid)
         assertEquals(matchingId, confirmResponse.matchingId)
-     */
+    }
 }
