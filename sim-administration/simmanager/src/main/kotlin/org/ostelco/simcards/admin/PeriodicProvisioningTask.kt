85e03ab19a6330cd1df22177341ad86c412a5788
Bjørn Remseth <la3lma@gmail.com>
2019-09-17 23:42:18 +0200

    Remove empty init block

--- sim-administration/simmanager/src/main/kotlin/org/ostelco/simcards/admin/PeriodicProvisioningTask.kt
+++ sim-administration/simmanager/src/main/kotlin/org/ostelco/simcards/admin/PeriodicProvisioningTask.kt
@@ -50,11 +50,7 @@ class PreallocateProfilesTask(
 
 
     private val logger by getLogger()
-
-
-    init {
-
-    }
+    
 
     @Throws(Exception::class)
     override fun execute(parameters: ImmutableMultimap<String, String>, output: PrintWriter) {
