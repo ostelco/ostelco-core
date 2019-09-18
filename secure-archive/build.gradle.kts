00ac95c808d6da486c9ea08b1b1adce4ef271b46
Vihang Patil <vihang.patil@gmail.com>
2019-09-18 16:19:57 +0200

    Ported secure-archive build.gralde to kts.

---
+++ secure-archive/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+  kotlin("jvm")
+  `java-library`
+}
+
+dependencies {
+
+  val jacksonVersion:String by rootProject.extra
+  val dropwizardVersion:String by rootProject.extra
+  val googleCloudVersion:String by rootProject.extra
+  val tinkVersion:String by rootProject.extra
+  val kotlinVersion:String by rootProject.extra
+
+  implementation(project(":prime-modules"))
+  
+  implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+
+  implementation("io.dropwizard:dropwizard-client:$dropwizardVersion")
+  implementation("com.google.cloud:google-cloud-datastore:$googleCloudVersion")
+  implementation("com.google.cloud:google-cloud-storage:$googleCloudVersion")
+  implementation("com.google.crypto.tink:tink:$tinkVersion")
+
+  testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
+
+apply(from = "../gradle/jacoco.gradle")
