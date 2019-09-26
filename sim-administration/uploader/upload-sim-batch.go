52c8e8c3fce1b73c183186f1e107eaab66af577a
Bjørn Remseth <la3lma@gmail.com>
2019-09-26 16:34:01 +0200

    move procedures to a better place in the file

--- sim-administration/uploader/upload-sim-batch.go
+++ sim-administration/uploader/upload-sim-batch.go
@@ -175,6 +175,24 @@ func trimSuffix(s string, suffixLen int) string {
 	return s[:len(s)-suffixLen]
 }
 
+func Sign(x int) int {
+	if x < 0 {
+		return -1
+	} else if x > 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+// Abs returns the absolute value of x.
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 func parseCommandLine() Batch {
 	//
 	// Set up command line parsing
@@ -278,22 +296,4 @@ func parseCommandLine() Batch {
 		firstMsisdn:     firstMsisdnInt,
 		msisdnIncrement: Sign(msisdnLen),
 	}
-}
-
-func Sign(x int) int {
-	if x < 0 {
-		return -1
-	} else if x > 0 {
-		return 1
-	} else {
-		return 0
-	}
-}
-
-// Abs returns the absolute value of x.
-func Abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
 }
