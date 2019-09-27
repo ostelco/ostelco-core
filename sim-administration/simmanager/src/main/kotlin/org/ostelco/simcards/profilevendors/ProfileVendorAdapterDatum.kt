e7ea433762eda4c1ee48bf541f730264198e0dc0
Bjørn Remseth <la3lma@gmail.com>
2019-09-27 21:05:16 +0200

    Whitespace

--- sim-administration/simmanager/src/main/kotlin/org/ostelco/simcards/profilevendors/ProfileVendorAdapterDatum.kt
+++ sim-administration/simmanager/src/main/kotlin/org/ostelco/simcards/profilevendors/ProfileVendorAdapterDatum.kt
@@ -143,7 +143,7 @@ data class ProfileVendorAdapter(
                         if (result == null) {
                             logAndReturnNotFoundError("Couldn't find any response for query iccidlist='$iccidList'")
                         } else {
-                             result.right()
+                            result.right()
                         }
                     }
                 }
