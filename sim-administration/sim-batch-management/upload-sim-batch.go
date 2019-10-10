ab4473c8de823f3dd650312c8caf9d8f9946de97
Bjørn Remseth <la3lma@gmail.com>
2019-10-10 20:31:26 +0200

    Add TODO

--- sim-administration/sim-batch-management/upload-sim-batch.go
+++ sim-administration/sim-batch-management/upload-sim-batch.go
@@ -6,6 +6,9 @@ import "github.com/ostelco/ostelco-core/sim-administration/sim-batch-management/
 
 func main() {
 	batch := uploadtoprime.ParseUploadFileGeneratorCommmandline()
+
+	// TODO: Combine these two into something inside uploadtoprime.
+	//       It's unecessary to break the batch thingy open in this way.
 	var csvPayload = uploadtoprime.GenerateCsvPayload(batch)
 
 	uploadtoprime.GeneratePostingCurlscript(batch.Url, csvPayload)
