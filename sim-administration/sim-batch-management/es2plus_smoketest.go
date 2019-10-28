e41e86d22dbfda5eff556ff20061cd25c67fd7b9
Bjørn Remseth <la3lma@gmail.com>
2019-10-28 15:18:09 +0100

    Hole in the wall now established, moving on.

---
+++ sim-administration/sim-batch-management/es2plus_smoketest.go
@@ -0,0 +1,54 @@
+//usr/bin/env go run "$0" "$@"; exit "$?"
+package main
+
+
+import (
+"crypto/tls"
+"crypto/x509"
+"fmt"
+"io"
+"log"
+"flag"
+)
+
+// ES2PLUS_ENDPOINT="https://mconnect-es2-005.staging.oberthur.net:1034"f
+
+func main() {
+
+	certFilePath := flag.String("cert", "", "Certificate pem file.")
+	keyFilePath := flag.String("key", "", "Certificate key file.")
+	hostport := flag.String("hostport", "", "host:port of ES2+ endpoint.")
+
+	flag.Parse()
+
+	cert, err := tls.LoadX509KeyPair(
+		*certFilePath,
+		*keyFilePath)
+	if err != nil {
+		log.Fatalf("server: loadkeys: %s", err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", *hostport, &config)
+	if err != nil {
+		log.Fatalf("client: dial: %s", err)
+	}
+	defer conn.Close()
+	log.Println("client: connected to: ", conn.RemoteAddr())
+	state := conn.ConnectionState()
+	for _, v := range state.PeerCertificates {
+		fmt.Println("Client: Server public key is:")
+		fmt.Println(x509.MarshalPKIXPublicKey(v.PublicKey))
+	}
+	log.Println("client: handshake: ", state.HandshakeComplete)
+	log.Println("client: mutual: ", state.NegotiatedProtocolIsMutual)
+	message := "Hello\n"
+	n, err := io.WriteString(conn, message)
+	if err != nil {
+		log.Fatalf("client: write: %s", err)
+	}
+	log.Printf("client: wrote %q (%d bytes)", message, n)
+	reply := make([]byte, 256)
+	n, err = conn.Read(reply)
+	log.Printf("client: read %q (%d bytes)", string(reply[:n]), n)
+	log.Print("client: exiting")
+}
