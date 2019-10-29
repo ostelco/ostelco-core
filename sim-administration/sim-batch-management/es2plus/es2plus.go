d3c30c6a357ef80aeb7fc508783a7e88ce17ab7f
Bjørn Remseth <la3lma@gmail.com>
2019-10-29 14:27:49 +0100

    Now we have a somewhat functioning library

---
+++ sim-administration/sim-batch-management/es2plus/es2plus.go
@@ -0,0 +1,188 @@
+package es2plus
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+///
+///  Generic headers for invocations and responses
+///
+
+type ES2PlusHeader struct {
+	FunctionRequesterIdentifier string `json:"functionRequesterIdentifier"`
+	FunctionCallIdentifier      string `json:"functionCallIdentifier"`
+}
+
+type ES2PlusGetProfileStatusRequest struct {
+	Header    ES2PlusHeader  `json:"header"`
+	IccidList []ES2PlusIccid `json:"iccidList"`
+}
+
+type ES2PlusIccid struct {
+	Iccid string `json:"iccid"`
+}
+
+type FunctionExecutionStatus struct {
+	FunctionExecutionStatusType string                `json:"status"` // Should be an enumeration type
+	StatusCodeData              ES2PlusStatusCodeData `json:"statusCodeData"`
+}
+
+
+type ES2PlusResponseHeader struct {
+	FunctionExecutionStatus FunctionExecutionStatus `json:"functionExecutionStatus"`
+}
+
+//
+//  Status code invocation.
+//
+
+type ES2PlusStatusCodeData struct {
+	SubjectCode       string `json:"subjectCode"`
+	ReasonCode        string `json:"reasonCode"`
+	SubjectIdentifier string `json:"subjectIdentifier"`
+	Message           string `json:"message"`
+}
+
+type ES2ProfileStatusResponse struct {
+	Header              ES2PlusResponseHeader `json:"header"`
+	ProfileStatusList   []ProfileStatus       `json:"profileStatusList"`
+	CompletionTimestamp string                `json:"completionTimestamp"`
+}
+
+type ProfileStatus struct {
+	StatusLastUpdateTimestamp string `json:"status_last_update_timestamp"`
+	ACToken                   string `json:"acToken"`
+	State                     string `json:"state"`
+	Eid                       string `json:"eid"`
+	Iccid                     string `json:"iccid"`
+	LockFlag                  bool   `json:"lockFlag"`
+}
+
+///
+///  Protocol code
+///
+
+func NewStatusRequest(iccid string, functionRequesterIdentifier string, functionCallIdentifier string) ES2PlusGetProfileStatusRequest {
+	return ES2PlusGetProfileStatusRequest{
+		Header:    ES2PlusHeader{FunctionCallIdentifier: functionCallIdentifier, FunctionRequesterIdentifier: functionRequesterIdentifier},
+		IccidList: [] ES2PlusIccid{ES2PlusIccid{Iccid: iccid}},
+	}
+}
+
+// formatRequest generates ascii representation of a request
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	// Return the request as a string
+	return strings.Join(request, "\n")
+}
+
+
+
+func getProfileInfo(certFilePath string, keyFilePath string, hostport string, requesterId string, iccid string, functionCallIdentifier string) (*ES2ProfileStatusResponse, error) {
+	client := NewClient(certFilePath, keyFilePath)
+	es2plusCommand := "getProfileStatus"
+
+	payload := NewStatusRequest(iccid, requesterId, functionCallIdentifier)
+	jsonStrB, err := json.Marshal(&payload)
+	if err != nil {
+		return nil, err
+	}
+
+	responseBytes, err := executeGenericEs2plusCommand(jsonStrB, hostport, es2plusCommand,  client)
+	if err != nil {
+		return nil, err
+	}
+
+	var result = new(ES2ProfileStatusResponse)
+	err = json.Unmarshal(responseBytes, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, err
+}
+
+func MarshalUnmarshalGeneriEs2plusCommand(client *http.Client, hostport string, es2plusCommand string,  payload interface{}, result interface{}) error {
+
+
+	jsonStrB, err := json.Marshal(payload)
+	if err != nil {
+		return  err
+	}
+
+	responseBytes, err := executeGenericEs2plusCommand(jsonStrB, hostport, es2plusCommand,  client)
+	if err != nil {
+		return  err
+	}
+
+	err = json.Unmarshal(responseBytes, result)
+	return err
+}
+
+
+func executeGenericEs2plusCommand(jsonStrB []byte, hostport string, es2plusCommand string, client *http.Client) ([]byte, error) {
+	fmt.Println(string(jsonStrB))
+	url := fmt.Sprintf("https://%s/gsma/rsp2/es2plus/%s", hostport, es2plusCommand)
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStrB))
+	req.Header.Set("X-Admin-Protocol", "gsma/rsp/v2.0.0")
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	// TODO Should check response headers here!
+	// (in particular X-admin-protocol)
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	responseBytes := []byte(response)
+	return responseBytes, nil
+}
+
+// TODO:   This is now just a http client, but we should extend the _external_ interface
+//         generate a es2plus endpoint, that contains the endpoint url, and a generator
+//         for function invocation IDs.  This will also require som reengineering of the
+//         rest of the API.
+func NewClient(certFilePath string, keyFilePath string) *http.Client {
+	cert, err := tls.LoadX509KeyPair(
+		certFilePath,
+		keyFilePath)
+	if err != nil {
+		log.Fatalf("server: loadkeys: %s", err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &config,
+		},
+	}
+	return client
+}
+
--- sim-administration/sim-batch-management/es2plus_smoketest.go
+++ sim-administration/sim-batch-management/es2plus_smoketest.go
@@ -2,188 +2,10 @@
 package main
 
 import (
-	"bytes"
-	"crypto/tls"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strings"
+	"github.com/ostelco/ostelco-core/sim-administration/sim-batch-management/es2plus"
 )
-
-///
-///  Generic headers for invocations and responses
-///
-
-type ES2PlusHeader struct {
-	FunctionRequesterIdentifier string `json:"functionRequesterIdentifier"`
-	FunctionCallIdentifier      string `json:"functionCallIdentifier"`
-}
-
-type ES2PlusGetProfileStatusRequest struct {
-	Header    ES2PlusHeader  `json:"header"`
-	IccidList []ES2PlusIccid `json:"iccidList"`
-}
-
-type ES2PlusIccid struct {
-	Iccid string `json:"iccid"`
-}
-
-type FunctionExecutionStatus struct {
-	FunctionExecutionStatusType string                `json:"status"` // Should be an enumeration type
-	StatusCodeData              ES2PlusStatusCodeData `json:"statusCodeData"`
-}
-
-
-type ES2PlusResponseHeader struct {
-	FunctionExecutionStatus FunctionExecutionStatus `json:"functionExecutionStatus"`
-}
-
-
-//
-//  Status code invocation.
-//
-
-type ES2PlusStatusCodeData struct {
-	SubjectCode       string `json:"subjectCode"`
-	ReasonCode        string `json:"reasonCode"`
-	SubjectIdentifier string `json:"subjectIdentifier"`
-	Message           string `json:"message"`
-}
-
-type ES2ProfileStatusResponse struct {
-	Header              ES2PlusResponseHeader `json:"header"`
-	ProfileStatusList   []ProfileStatus       `json:"profileStatusList"`
-	CompletionTimestamp string                `json:"completionTimestamp"`
-}
-
-type ProfileStatus struct {
-	StatusLastUpdateTimestamp string `json:"status_last_update_timestamp"`
-	ACToken                   string `json:"acToken"`
-	State                     string `json:"state"`
-	Eid                       string `json:"eid"`
-	Iccid                     string `json:"iccid"`
-	LockFlag                  bool   `json:"lockFlag"`
-}
-
-///
-///  Protocol code
-///
-
-func newStatusRequest(iccid string, functionRequesterIdentifier string, functionCallIdentifier string) ES2PlusGetProfileStatusRequest {
-	return ES2PlusGetProfileStatusRequest{
-		Header:    ES2PlusHeader{FunctionCallIdentifier: functionCallIdentifier, FunctionRequesterIdentifier: functionRequesterIdentifier},
-		IccidList: [] ES2PlusIccid{ES2PlusIccid{Iccid: iccid}},
-	}
-}
-
-// formatRequest generates ascii representation of a request
-func formatRequest(r *http.Request) string {
-	// Create return string
-	var request []string
-	// Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-	// Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
-	// Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	}
-	// Return the request as a string
-	return strings.Join(request, "\n")
-}
-
-
-
-func getProfileInfo(certFilePath string, keyFilePath string, hostport string, requesterId string, iccid string, functionCallIdentifier string) (*ES2ProfileStatusResponse, error) {
-	client := newClient(certFilePath, keyFilePath)
-	es2plusCommand := "getProfileStatus"
-
-	payload := newStatusRequest(iccid, requesterId, functionCallIdentifier)
-	jsonStrB, err := json.Marshal(&payload)
-	if err != nil {
-		return nil, err
-	}
-
-	responseBytes, err := executeGenericEs2plusCommand(jsonStrB, hostport, es2plusCommand,  client)
-	if err != nil {
-		return nil, err
-	}
-
-	var result = new(ES2ProfileStatusResponse)
-	err = json.Unmarshal(responseBytes, &result)
-	if err != nil {
-		return nil, err
-	}
-	return result, err
-}
-
-func marshalUnmarshalGeneriEs2plusCommand(client *http.Client, hostport string, es2plusCommand string,  payload interface{}, result interface{}) error {
-
-
-	jsonStrB, err := json.Marshal(payload)
-	if err != nil {
-		return  err
-	}
-
-	responseBytes, err := executeGenericEs2plusCommand(jsonStrB, hostport, es2plusCommand,  client)
-	if err != nil {
-		return  err
-	}
-
-	err = json.Unmarshal(responseBytes, result)
-	return err
-}
-
-
-func executeGenericEs2plusCommand(jsonStrB []byte, hostport string, es2plusCommand string, client *http.Client) ([]byte, error) {
-	fmt.Println(string(jsonStrB))
-	url := fmt.Sprintf("https://%s/gsma/rsp2/es2plus/%s", hostport, es2plusCommand)
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStrB))
-	req.Header.Set("X-Admin-Protocol", "gsma/rsp/v2.0.0")
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	// TODO Should check response headers here!
-	// (in particular X-admin-protocol)
-	response, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	responseBytes := []byte(response)
-	return responseBytes, nil
-}
-
-func newClient(certFilePath string, keyFilePath string) *http.Client {
-	cert, err := tls.LoadX509KeyPair(
-		certFilePath,
-		keyFilePath)
-	if err != nil {
-		log.Fatalf("server: loadkeys: %s", err)
-	}
-	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &config,
-		},
-	}
-	return client
-}
 
 
 ///
@@ -206,12 +28,12 @@ func main() {
 
 	flag.Parse()
 
-	result :=  new(ES2ProfileStatusResponse)
+	result :=  new(es2plus.ES2ProfileStatusResponse)
 	functionCallIdentifier := "kadkjfad"
 	iccid := "8965030119040000067"
-	statusRequest := newStatusRequest(iccid, *requesterId, functionCallIdentifier)
-	client := newClient(*certFilePath, *keyFilePath)
-	err:= marshalUnmarshalGeneriEs2plusCommand(client,   *hostport, "getProfileStatus", statusRequest, result)
+	statusRequest := es2plus.NewStatusRequest(iccid, *requesterId, functionCallIdentifier)
+	client := es2plus.NewClient(*certFilePath, *keyFilePath)
+	err:= es2plus.MarshalUnmarshalGeneriEs2plusCommand(client,   *hostport, "getProfileStatus", statusRequest, result)
 	if err != nil {
 		panic(err)
 	}
