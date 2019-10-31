a18e4710cfe8c62ce23f72c82029edbcb3d39f6e
Prasanth Ullattil <prasanth@telenordigital.com>
2019-10-31 15:06:35 +0100

    Simplify Customer List component

--- houston/src/components/Search/CustomerList.js
+++ houston/src/components/Search/CustomerList.js
@@ -6,43 +6,31 @@ import Highlighter from "react-highlight-words";
 
 import { subscriberActions } from '../../actions/subscriber.actions';
 
-const Highlight = ({ children, highlightIndex }) => (
+const Highlight = ({ children }) => (
   <strong className="bg-warning text-dark">{children}</strong>
 );
 
-const convertToHighlightedText = (text, query) => {
-  return (
-    <Highlighter
-      searchWords={[query]}
-      autoEscape={true}
-      caseSensitive={true}
-      textToHighlight={text}
-      highlightTag={Highlight}
-    />);
-}
+const convertToHighlightedText = (text, query) => (
+  <Highlighter
+    searchWords={[query]}
+    autoEscape={true}
+    caseSensitive={true}
+    textToHighlight={text}
+    highlightTag={Highlight}
+  />);
 
-export const CustomerRow = props => {
-
-  let query = localStorage.getItem('searchedEmail')
-  function onSelect(e) {
-    e.preventDefault();
-    console.log(`Selecting customer with id ${props.customer.id}`);
-    props.selectCustomer(props.customer);
-  }
-
-  return (
-    <Card>
-      <CardBody>
-        <CardText>
-          {convertToHighlightedText(props.customer.nickname, query)}<br />
-          {convertToHighlightedText(props.customer.contactEmail, query)}<br />
-          {convertToHighlightedText(props.customer.id, query)}<br />
-          <br />
-          <Button color="primary" onClick={onSelect}>{'Show details'}</Button>
-        </CardText>
-      </CardBody>
-    </Card>);
-}
+const CustomerRow = ({ customer, select, query }) => (
+  <Card>
+    <CardBody>
+      <CardText>
+        {convertToHighlightedText(customer.nickname, query)}<br />
+        {convertToHighlightedText(customer.contactEmail, query)}<br />
+        {convertToHighlightedText(customer.id, query)}<br />
+        <br />
+        <Button color="primary" onClick={() => select(customer)}>{'Show details'}</Button>
+      </CardText>
+    </CardBody>
+  </Card>);
 
 CustomerRow.propTypes = {
   customer: PropTypes.shape({
@@ -50,24 +38,28 @@ CustomerRow.propTypes = {
     nickname: PropTypes.string,
     contactEmail: PropTypes.string,
   }),
-  selectCustomer: PropTypes.func.isRequired
+  query: PropTypes.string,
+  select: PropTypes.func.isRequired
 };
 
-export const CustomerList = props => {
+const CustomerList = props => {
+  let query = localStorage.getItem('searchedEmail')
+
   // If customer is set, remove the list.
   if (props.customer.id || !Array.isArray(props.subscribers)) {
     return null;
   }
-  let listItems = null;
-  if (Array.isArray(props.subscribers)) {
-    listItems = props.subscribers.map((customer, index) =>
-      <div key={index}>
-        <CustomerRow customer={customer} selectCustomer={props.selectCustomer} key={index} />
-        <br />
 
-      </div>
-    );
-  }
+  const listItems = props.subscribers.map((customer, index) =>
+    <div key={index}>
+      <CustomerRow
+        customer={customer}
+        query={query}
+        select={props.selectCustomer}
+        key={index} />
+      <br />
+    </div>
+  );
   return (
     <div>
       <h6>Found following matching records... </h6>
@@ -82,7 +74,7 @@ CustomerList.propTypes = {
     PropTypes.array
   ]),
   customer: PropTypes.object,
-  selectCustomer: PropTypes.func.isRequired
+  select: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) {
@@ -94,6 +86,6 @@ function mapStateToProps(state) {
   };
 }
 const mapDispatchToProps = {
-  selectCustomer: subscriberActions.selectCustomer
+  select: subscriberActions.selectCustomer
 }
 export default connect(mapStateToProps, mapDispatchToProps)(CustomerList);
