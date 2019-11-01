ad31c217be573925ca71002321f199eb9cefe2d4
Prasanth Ullattil <prasanth@telenordigital.com>
2019-11-01 13:51:23 +0100

    Simplify Notification editor using redux hooks

--- houston/src/components/Notifications/NotificationEditor.js
+++ houston/src/components/Notifications/NotificationEditor.js
@@ -1,61 +1,45 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 import { notifyActions } from '../../actions/notifiy.actions';
+// This uses redux hooks
+// https://react-redux.js.org/api/hooks
 
-function NotificationEditor(props) {
+function NotificationEditor({ messageLabel, submitLabel, titleLabel }) {
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.notification);
+  const { message, title } = notification;
+
   function onSubmit(e) {
     e.preventDefault();
-    props.sendNotificationToSubscriber(props.title, props.message);
+    dispatch(notifyActions.sendNotificationToSubscriber(title, message));
   }
   return (
     <Form onSubmit={onSubmit}>
       <FormGroup>
-        <Label for="inputTitle">{props.titleLabel}</Label>
+        <Label for="inputTitle">{titleLabel}</Label>
         <Input
           name="text"
           id="inputTitle"
-          value={props.title}
-          onChange={(e) => props.setNotificationTitle(e.target.value)}
+          value={title}
+          onChange={(e) => dispatch(notifyActions.setNotificationTitle(e.target.value))}
           placeholder="Enter title"
         />
       </FormGroup>
       <FormGroup>
-        <Label for="inputMessage">{props.messageLabel}</Label>
+        <Label for="inputMessage">{messageLabel}</Label>
         <Input
           type="textarea"
           name="text"
           id="inputMessage"
-          value={props.message}
-          onChange={(e) => props.setNotificationMessage(e.target.value)}
+          value={message}
+          onChange={(e) => dispatch(notifyActions.setNotificationMessage(e.target.value))}
           placeholder="Enter message"
         />
       </FormGroup>
-      <Button bsstyle="primary" type="submit">{props.submitLabel}</Button>
+      <Button bsstyle="primary" type="submit">{submitLabel}</Button>
     </Form>
   );
 }
-
-NotificationEditor.propTypes = {
-  titleLabel: PropTypes.string.isRequired,
-  messageLabel: PropTypes.string.isRequired,
-  submitLabel: PropTypes.string.isRequired,
-};
-
-function mapStateToProps(state) {
-  let notification = state.notification;
-  return {
-    message: notification.message,
-    title: notification.title,
-    type: notification.type
-  };
-}
-const mapDispatchToProps = {
-  setNotificationMessage: notifyActions.setNotificationMessage,
-  setNotificationTitle: notifyActions.setNotificationTitle,
-  setNotificationType: notifyActions.setNotificationType,
-  sendNotificationToSubscriber: notifyActions.sendNotificationToSubscriber
-}
-export default connect(mapStateToProps, mapDispatchToProps)(NotificationEditor);
+export default NotificationEditor;
