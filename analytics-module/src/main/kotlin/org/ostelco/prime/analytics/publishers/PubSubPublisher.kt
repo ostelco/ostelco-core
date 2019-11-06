ee8f558407812ea825e60ca6cee680397383f402
Prasanth Ullattil <prasanth@telenordigital.com>
2019-11-06 10:56:18 +0100

    Fix compile error

--- analytics-module/src/main/kotlin/org/ostelco/prime/analytics/publishers/PubSubPublisher.kt
+++ analytics-module/src/main/kotlin/org/ostelco/prime/analytics/publishers/PubSubPublisher.kt
@@ -2,11 +2,9 @@ package org.ostelco.prime.analytics.publishers
 
 import com.google.pubsub.v1.PubsubMessage
 import io.dropwizard.lifecycle.Managed
-import java.util.concurrent.ScheduledExecutorService
 import org.ostelco.common.publisherex.Event
 
 interface PubSubPublisher1 : Managed {
-    var singleThreadScheduledExecutor: ScheduledExecutorService
     fun publishPubSubMessage(pubsubMessage: PubsubMessage)
     fun publishEvent(event: Event)
 }
