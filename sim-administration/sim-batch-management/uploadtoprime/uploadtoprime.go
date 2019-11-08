b9ae33feb973fc28c9a22fbbb64262292b5deb6a
Bjørn Remseth <la3lma@gmail.com>
2019-11-08 19:19:48 +0100

    Nuke code no longer being used

--- sim-administration/sim-batch-management/uploadtoprime/uploadtoprime.go
+++ sim-administration/sim-batch-management/uploadtoprime/uploadtoprime.go
@@ -2,10 +2,8 @@ package uploadtoprime
 
 import (
 	"fmt"
-	"github.com/ostelco/ostelco-core/sim-administration/sim-batch-management/fieldsyntaxchecks"
 	"github.com/ostelco/ostelco-core/sim-administration/sim-batch-management/model"
 	"github.com/ostelco/ostelco-core/sim-administration/sim-batch-management/store"
-	"strconv"
 	"strings"
 )
 
@@ -15,41 +13,6 @@ func GeneratePostingCurlscript(url string, payload string) {
 	fmt.Printf("curl  -H 'Content-Type: text/plain' -X PUT --data-binary @-  %s <<EOF\n", url)
 	fmt.Printf("%s", payload)
 	fmt.Print("EOF\n")
-}
-
-func GenerateCsvPayload2(batch model.Batch) string {
-	var sb strings.Builder
-	sb.WriteString("ICCID, IMSI, MSISDN, PIN1, PIN2, PUK1, PUK2, PROFILE\n")
-
-	iccidWithoutLuhnChecksum, err :=  strconv.Atoi(batch.FirstIccid)
-	if err != nil {
-		panic(err)
-	}
-
-	imsi, err :=  strconv.Atoi(batch.FirstImsi)
-	if err != nil {
-		panic(err)
-	}
-
-	var msisdn, err2 =  strconv.Atoi(batch.FirstMsisdn)
-	if err2 != nil {
-		panic(err)
-	}
-
-
-
-	// TODO: Replace with a loop over actual entries
-	for i := 0; i < batch.Quantity; i++ {
-		iccid := fmt.Sprintf("%d%1d", iccidWithoutLuhnChecksum, fieldsyntaxchecks.LuhnChecksum(iccidWithoutLuhnChecksum))
-		line := fmt.Sprintf("%s, %d, %d,,,,,%s\n", iccid, imsi, msisdn, batch.ProfileType)
-		sb.WriteString(line)
-
-		iccidWithoutLuhnChecksum += batch.IccidIncrement
-		imsi += batch.ImsiIncrement
-		msisdn += batch.MsisdnIncrement
-	}
-
-	return sb.String()
 }
 
 func GenerateCsvPayload3(db *store.SimBatchDB, batch model.Batch) string {
