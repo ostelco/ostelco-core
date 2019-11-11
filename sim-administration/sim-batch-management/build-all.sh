4f3e168fb3acfb2719002471e75ae58c06532e1d
Bjørn Remseth <la3lma@gmail.com>
2019-11-11 08:56:52 +0100

    Add static analysis to build-all.sh

--- sim-administration/sim-batch-management/build-all.sh
+++ sim-administration/sim-batch-management/build-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
+
+
+# First we want the thing to compile
 go build
 
 if [ "$?" -ne "0" ]; then
@@ -10,12 +13,18 @@ fi
 
 
 
-
+# THen to pass tests
 go test ./...
 
 if [ "$?" -ne "0" ]; then
   echo "Sorry, one or more tests failed, aborting build."
   exit 1
 fi
+
+# Then... 
+# somewhat  nonportably ... run static analysis of the
+# go code.
+
+ ~/go/bin/staticcheck ./...
 
 
