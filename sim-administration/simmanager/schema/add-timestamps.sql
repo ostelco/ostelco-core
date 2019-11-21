1d73073d1a26a779781b16f5b05c6883fe625c05
Kjell M. Myksvoll <kmm@comoyo.com>
2019-11-21 15:11:53 +0100

    Improved version of Postgres sql script to add timestamps

--- sim-administration/simmanager/schema/add-timestamps.sql
+++ sim-administration/simmanager/schema/add-timestamps.sql
@@ -2,55 +2,52 @@
 -- Add timstamp column for hlrstate
 --
 alter table sim_entries add column tshlrstate timestamp;
-alter table sim_entries alter column tshlrstate set not null default now();
+alter table sim_entries alter column tshlrstate set default now();
 create or replace function hlrstate_changed()
 returns trigger as $$
 begin
-  if new.hlrstate != old.hlrstate then
-    new.tshlrstate := now();
-  end if;
-return new;
+  new.tshlrstate := now();
+  return new;
 end;
 $$ language plpgsql;
 create trigger trigger_hlrstate
 before update on sim_entries
 for each row
+when (old.hlrstate is distinct from new.hlrstate)
 execute procedure hlrstate_changed();
 
 --
 -- Add timstamp column for smdpplusstate
 --
 alter table sim_entries add column tssmdpplusstate timestamp;
-alter table sim_entries alter column tssmdpplusstate not null set default now();
+alter table sim_entries alter column tssmdpplusstate set default now();
 create or replace function smdpplusstate_changed()
 returns trigger as $$
 begin
-  if new.smdpplusstate != old.smdpplusstate then
-    new.tssmdpplusstate := now();
-  end if;
-return new;
+  new.tssmdpplusstate := now();
+  return new;
 end;
 $$ language plpgsql;
 create trigger trigger_smdpplusstate
 before update on sim_entries
 for each row
+when (old.smdpplusstate is distinct from new.smdpplusstate)
 execute procedure smdpplusstate_changed();
 
 --
 -- Add timstamp column for provisionstate
 --
 alter table sim_entries add column tsprovisionstate timestamp;
-alter table sim_entries alter column tsprovisionstate not null set default now();
+alter table sim_entries alter column tsprovisionstate set default now();
 create or replace function provisionstate_changed()
 returns trigger as $$
 begin
-  if new.provisionstate != old.provisionstate then
-    new.tsprovisionstate := now();
-  end if;
-return new;
+  new.tsprovisionstate := now();
+  return new;
 end;
 $$ language plpgsql;
 create trigger trigger_provisionstate
 before update on sim_entries
 for each row
+when (old.provisionstate is distinct from new.provisionstate)
 execute procedure provisionstate_changed();
