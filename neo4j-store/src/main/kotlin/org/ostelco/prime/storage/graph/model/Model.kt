6428f385f42d0068c2f311da509328e46c4b0950
Vihang Patil <vihang.patil@gmail.com>
2019-11-22 22:15:19 +0100

    Review comments

--- neo4j-store/src/main/kotlin/org/ostelco/prime/storage/graph/model/Model.kt
+++ neo4j-store/src/main/kotlin/org/ostelco/prime/storage/graph/model/Model.kt
@@ -5,6 +5,19 @@ import org.ostelco.prime.model.HasId
 import org.ostelco.prime.model.KycStatus
 import org.ostelco.prime.model.KycType
 
+/*
+  This are the Neo4jStore's internal entity classes, which are almost similar to their prime model's counterparts.
+  These classes have been given the same name of their prime model's counterparts as a convention.
+  These classes exists because for some of the prime model's entity classes cannot be stored as it is in Neo4j.
+  Some of the reasons why that is done so is:
+  1. A single class in prime model gets split into 2 classes in Graph DB - one for entity and one for relation.
+     E.g. [Identity] and [Identifies].
+  2. A single class in prime model is composition for multiple entities in Graph DB.
+     E.g. [Segment] and [Offer].
+  3. Graph DB has partial information compared to prime model's class.
+     E.g. [SimProfile] in Graph DB does not have SimStatus and ActivationCode, which is stored in Sim Manager.
+*/
+
 data class Identity(
         override val id: String,
         val type: String) : HasId {
